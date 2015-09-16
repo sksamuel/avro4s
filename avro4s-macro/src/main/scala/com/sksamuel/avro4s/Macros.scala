a6e06eb9342781b2814b5dcdf9e9f21522e4b7a0
sksamuel <sam@sksamuel.com>
2015-09-16 20:14:38 +0100

    Generating types from scala type signature

--- avro4s-macro/src/main/scala/com/sksamuel/avro4s/Macros.scala
+++ avro4s-macro/src/main/scala/com/sksamuel/avro4s/Macros.scala
@@ -1,7 +1,6 @@
 package com.sksamuel.avro4s
 
 import org.apache.avro.{Schema, SchemaBuilder}
-import org.apache.avro.SchemaBuilder.FieldAssembler
 
 import scala.language.experimental.macros
 import scala.reflect.macros.Context
@@ -30,12 +29,36 @@ trait AvroSchemaWriter[T] {
   def schema: org.apache.avro.Schema
 }
 
+//object SchemaTypeMapper {
+//  def scalaTypeToSchemaType(klass: Class[T]): Schema.Type = {
+//    Schema.create(Schema.Type.INT)
+//  }
+//}
+
+object Schemas {
+  def schemaType(sig: String): Schema.Type = {
+    sig match {
+      case "String" => Schema.Type.STRING
+      case "Int" => Schema.Type.INT
+      case "Long" => Schema.Type.LONG
+      case "Boolean" => Schema.Type.BOOLEAN
+      case _ => Schema.Type.STRING
+    }
+  }
+
+  def schemaFields(xs: Seq[(String, String)]): List[Schema.Field] = {
+    xs.map { case (name, sig) =>
+      new Schema.Field(name, Schema.create(schemaType(sig)), s"Generated from scala type: $sig", null)
+    }.toList
+  }
+}
+
 object Macros {
   implicit def materializeWriter[T]: AvroSchemaWriter[T] = macro materializeWriterImpl[T]
 
   def materializeWriterImpl[T: c.WeakTypeTag](c: Context): c.Expr[AvroSchemaWriter[T]] = {
-
     import c.universe._
+
     val tpe = weakTypeOf[T]
     val companion = tpe.typeSymbol.companionSymbol
 
@@ -43,20 +66,15 @@ object Macros {
       case m: MethodSymbol if m.isPrimaryConstructor => m
     }.get.paramss.head
 
-    val a = SchemaBuilder.record(tpe.typeSymbol.asClass.fullName).fields()
-    val b = fields.map { field =>
-      val decoded = field.name.decoded
-      new Schema.Field(decoded, Schema.create(Schema.Type.INT), "", null)
-      q"""new org.apache.avro.Schema.Field($decoded, org.apache.avro.Schema.create(org.apache.avro.Schema.Type.INT), "", null)"""
-    }
+    val aa = fields.map { field => field.name.decoded -> field.typeSignature.toString }
 
     c.Expr[AvroSchemaWriter[T]] { q"""
       new AvroSchemaWriter[$tpe] {
         def schema : org.apache.avro.Schema = {
           import scala.collection.JavaConverters._
           val s = org.apache.avro.Schema.createRecord(${tpe.typeSymbol.fullName}, "", ${tpe.typeSymbol.fullName}, false)
-          val fields = (..$b).productIterator.asInstanceOf[scala.collection.Iterator[org.apache.avro.Schema.Field]]
-          s.setFields(fields.toList.asJava)
+          val fields = com.sksamuel.avro4s.Schemas.schemaFields($aa)
+          s.setFields(fields.asJava)
           s
         }
       }
