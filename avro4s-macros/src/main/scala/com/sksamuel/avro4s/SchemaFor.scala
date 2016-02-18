0f0f148c513f9eb74f1d07d016fc224654d85c7c
sksamuel <sam@sksamuel.com>
2016-02-18 20:15:59 +0000

    Simplified avro schema impl

--- avro4s-macros/src/main/scala/com/sksamuel/avro4s/SchemaFor.scala
+++ avro4s-macros/src/main/scala/com/sksamuel/avro4s/SchemaFor.scala
@@ -11,25 +11,18 @@ import scala.reflect.ClassTag
 import scala.reflect.macros.Context
 import scala.collection.JavaConverters._
 
-@implicitNotFound(msg = "Cannot find implicit ToSchema for ${T}")
-trait ToSchema[T] {
+@implicitNotFound("Cout not find implicit SchemaFor[${T}]")
+trait SchemaFor[T] {
   def apply(): org.apache.avro.Schema
 }
 
-trait LowPriorityToSchema {
+object SchemaFor  {
 
-  implicit def GenericToSchema[T: Manifest](implicit schemaFor: SchemaFor[T]): ToSchema[T] = new ToSchema[T] {
-    override def apply(): Schema = schemaFor()
-  }
-}
-
-object ToSchema extends LowPriorityToSchema {
-
-  implicit val BooleanToSchema: ToSchema[Boolean] = new ToSchema[Boolean] {
+  implicit val BooleanSchemaFor: SchemaFor[Boolean] = new SchemaFor[Boolean] {
     def apply(): Schema = Schema.create(Schema.Type.BOOLEAN)
   }
 
-  implicit val BigDecimalToSchema: ToSchema[BigDecimal] = new ToSchema[BigDecimal] {
+  implicit val BigDecimalSchemaFor: SchemaFor[BigDecimal] = new SchemaFor[BigDecimal] {
     def apply(): Schema = {
       val schema = Schema.create(Schema.Type.BYTES)
       schema.addProp("logicalType", "decimal")
@@ -39,92 +32,84 @@ object ToSchema extends LowPriorityToSchema {
     }
   }
 
-  implicit val ByteArraySchema: ToSchema[Array[Byte]] = new ToSchema[Array[Byte]] {
+  implicit val ByteArraySchema: SchemaFor[Array[Byte]] = new SchemaFor[Array[Byte]] {
     def apply(): Schema = Schema.create(Schema.Type.BYTES)
   }
 
-  implicit val DoubleToSchema: ToSchema[Double] = new ToSchema[Double] {
+  implicit val DoubleSchemaFor: SchemaFor[Double] = new SchemaFor[Double] {
     def apply(): Schema = Schema.create(Schema.Type.DOUBLE)
   }
 
-  implicit def EitherSchema[A, B](implicit aSchema: ToSchema[A], bSchema: ToSchema[B]): ToSchema[Either[A, B]] = {
-    new ToSchema[Either[A, B]] {
+  implicit def EitherSchema[A, B](implicit aSchema: SchemaFor[A], bSchema: SchemaFor[B]): SchemaFor[Either[A, B]] = {
+    new SchemaFor[Either[A, B]] {
       def apply(): Schema = Schema.createUnion(util.Arrays.asList(aSchema.apply, bSchema.apply))
     }
   }
 
-  implicit def EnumToSchema[E <: Enum[_]](implicit tag: ClassTag[E]): ToSchema[E] = new ToSchema[E] {
+  implicit def EnumSchemaFor[E <: Enum[_]](implicit tag: ClassTag[E]): SchemaFor[E] = new SchemaFor[E] {
     override def apply(): Schema = {
       val values = tag.runtimeClass.getEnumConstants.map(_.toString)
       Schema.createEnum(tag.runtimeClass.getSimpleName, null, tag.runtimeClass.getPackage.getName, values.toList.asJava)
     }
   }
 
-  implicit val FloatSchema: ToSchema[Float] = new ToSchema[Float] {
+  implicit val FloatSchema: SchemaFor[Float] = new SchemaFor[Float] {
     def apply(): Schema = Schema.create(Schema.Type.FLOAT)
   }
 
-  implicit val IntSchema: ToSchema[Int] = new ToSchema[Int] {
+  implicit val IntSchema: SchemaFor[Int] = new SchemaFor[Int] {
     def apply(): Schema = Schema.create(Schema.Type.INT)
   }
 
-  implicit val LongSchema: ToSchema[Long] = new ToSchema[Long] {
+  implicit val LongSchema: SchemaFor[Long] = new SchemaFor[Long] {
     def apply(): Schema = Schema.create(Schema.Type.LONG)
   }
 
-  implicit val StringToSchema: ToSchema[String] = new ToSchema[String] {
+  implicit val StringSchemaFor: SchemaFor[String] = new SchemaFor[String] {
     def apply(): Schema = Schema.create(Schema.Type.STRING)
   }
 
-  implicit def OptionToSchema[T](implicit valueSchema: ToSchema[T]): ToSchema[Option[T]] = {
-    new ToSchema[Option[T]] {
+  implicit def OptionSchemaFor[T](implicit valueSchema: SchemaFor[T]): SchemaFor[Option[T]] = {
+    new SchemaFor[Option[T]] {
       def apply(): Schema = Schema.createUnion(util.Arrays.asList(Schema.create(Schema.Type.NULL), valueSchema.apply))
     }
   }
 
-  implicit def ArrayToSchema[S](implicit subschema: ToSchema[S]): ToSchema[Array[S]] = {
-    new ToSchema[Array[S]] {
+  implicit def ArraySchemaFor[S](implicit subschema: SchemaFor[S]): SchemaFor[Array[S]] = {
+    new SchemaFor[Array[S]] {
       def apply(): Schema = Schema.createArray(subschema.apply)
     }
   }
 
-  implicit def IterableToSchema[S](implicit subschema: ToSchema[S]): ToSchema[Iterable[S]] = {
-    new ToSchema[Iterable[S]] {
+  implicit def IterableSchemaFor[S](implicit subschema: SchemaFor[S]): SchemaFor[Iterable[S]] = {
+    new SchemaFor[Iterable[S]] {
       def apply(): Schema = Schema.createArray(subschema.apply)
     }
   }
 
-  implicit def ListToSchema[S](implicit subschema: ToSchema[S]): ToSchema[List[S]] = {
-    new ToSchema[List[S]] {
+  implicit def ListSchemaFor[S](implicit subschema: SchemaFor[S]): SchemaFor[List[S]] = {
+    new SchemaFor[List[S]] {
       def apply(): Schema = Schema.createArray(subschema.apply)
     }
   }
 
-  implicit def SetToSchema[S](implicit subschema: ToSchema[S]): ToSchema[Set[S]] = {
-    new ToSchema[Set[S]] {
+  implicit def SetSchemaFor[S](implicit subschema: SchemaFor[S]): SchemaFor[Set[S]] = {
+    new SchemaFor[Set[S]] {
       def apply(): Schema = Schema.createArray(subschema.apply)
     }
   }
 
-  implicit def SeqToSchema[S](implicit subschema: ToSchema[S]): ToSchema[Seq[S]] = {
-    new ToSchema[Seq[S]] {
+  implicit def SeqSchemaFor[S](implicit subschema: SchemaFor[S]): SchemaFor[Seq[S]] = {
+    new SchemaFor[Seq[S]] {
       def apply(): Schema = Schema.createArray(subschema.apply)
     }
   }
 
-  implicit def MapToSchema[V](implicit valueSchema: ToSchema[V]): ToSchema[Map[String, V]] = {
-    new ToSchema[Map[String, V]] {
+  implicit def MapSchemaFor[V](implicit valueSchema: SchemaFor[V]): SchemaFor[Map[String, V]] = {
+    new SchemaFor[Map[String, V]] {
       def apply(): Schema = Schema.createMap(valueSchema.apply)
     }
   }
-}
-
-@implicitNotFound("Cout not find implicit SchemaFor[${T}]")
-trait SchemaFor[T] {
-  def apply(): org.apache.avro.Schema
-}
-
-object SchemaFor {
 
   implicit def apply[T]: SchemaFor[T] = macro applyImpl[T]
 
@@ -154,7 +139,7 @@ object SchemaFor {
         val schemas = fs map { f =>
           val sig = f.typeSignature
           q"""{
-            import com.sksamuel.avro4s.ToSchema._;
+            import com.sksamuel.avro4s.SchemaFor._;
             com.sksamuel.avro4s.SchemaFor.schemaBuilder[$sig]
           }
            """
@@ -174,7 +159,7 @@ object SchemaFor {
         val sig = f.typeSignature
         val annos = annotations(f)
         q"""{
-            import com.sksamuel.avro4s.ToSchema._;
+            import com.sksamuel.avro4s.SchemaFor._;
             com.sksamuel.avro4s.SchemaFor.fieldBuilder[$sig]($name, Seq(..$annos))
           }
        """
@@ -219,7 +204,7 @@ object SchemaFor {
   }
 
   // builds a schema for a type T
-  def schemaBuilder[T](implicit toSchema: ToSchema[T]): Schema = toSchema.apply
+  def schemaBuilder[T](implicit SchemaFor: SchemaFor[T]): Schema = SchemaFor.apply
 
   def unionBuilder(name: String, schemas: Set[Schema], optional: Boolean): Schema.Field = {
     val sortedSchemas = (if (optional) schemas + Schema.create(Schema.Type.NULL) else schemas).toSeq.sortBy(_.getName)
@@ -229,7 +214,7 @@ object SchemaFor {
   // given a name and a type T, builds the schema field for that type T. A schema field might itself contain
   // a nested record schema if T is a class. The provided annos are a wrapper around annotations.
   def fieldBuilder[T](name: String, annos: Seq[Anno])
-                     (implicit toSchema: ToSchema[T]): Schema.Field = fieldBuilder(name, annos, toSchema())
+                     (implicit SchemaFor: SchemaFor[T]): Schema.Field = fieldBuilder(name, annos, SchemaFor())
 
   def fieldBuilder(name: String, annos: Seq[Anno], schema: Schema): Schema.Field = {
     val field = new Schema.Field(name, schema, doc(annos), null)
