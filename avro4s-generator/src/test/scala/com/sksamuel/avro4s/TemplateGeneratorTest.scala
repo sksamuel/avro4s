59fdfdf91322046b630a25cd7a5254fdd35a02d2
Wytze Hazenberg <wytzehazenberg@gmail.com>
2016-07-18 15:07:29 +0200

    missing semicolon in test

--- avro4s-generator/src/test/scala/com/sksamuel/avro4s/TemplateGeneratorTest.scala
+++ avro4s-generator/src/test/scala/com/sksamuel/avro4s/TemplateGeneratorTest.scala
@@ -7,7 +7,7 @@ class TemplateGeneratorTest extends WordSpec with Matchers {
   "TemplateGenerator" should {
     "generate a file per enum" in {
       val enums = TemplateGenerator(Seq(EnumType("com.a", "Boo", Seq("A", "B")), EnumType("com.a", "Foo", Seq("A", "B"))))
-      enums shouldBe Seq(Template("com/a/Boo.java", "package com.a\n\n//auto generated code by avro4s\npublic enum Boo{\n    A, B\n}"), Template("com/a/Foo.java", "package com.a\n\n//auto generated code by avro4s\npublic enum Foo{\n    A, B\n}"))
+      enums shouldBe Seq(Template("com/a/Boo.java", "package com.a;\n\n//auto generated code by avro4s\npublic enum Boo{\n    A, B\n}"), Template("com/a/Foo.java", "package com.a;\n\n//auto generated code by avro4s\npublic enum Foo{\n    A, B\n}"))
     }
     "generate one file for all records of same namespace" in {
       val enums = TemplateGenerator(Seq(RecordType("com.a", "Boo", Seq(FieldDef("name", PrimitiveType.String), FieldDef("bibble", PrimitiveType.Long))), RecordType("com.a", "Foo", Seq(FieldDef("dibble", PrimitiveType.Double), FieldDef("bibble", PrimitiveType.Boolean)))))
