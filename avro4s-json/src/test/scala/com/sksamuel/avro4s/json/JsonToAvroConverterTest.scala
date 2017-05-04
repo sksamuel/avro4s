d34a7f9e59a2dca53b3a0219b7e0c44135c29082
netanelrabinowitz <netanel.r@youappi.com>
2017-05-04 03:30:06 +0300

    support json converting for different naming strategies jsons

--- avro4s-json/src/test/scala/com/sksamuel/avro4s/json/JsonToAvroConverterTest.scala
+++ avro4s-json/src/test/scala/com/sksamuel/avro4s/json/JsonToAvroConverterTest.scala
@@ -29,11 +29,35 @@ class JsonToAvroConverterTest extends WordSpec with Matchers {
       val expected = new org.apache.avro.Schema.Parser().parse(getClass.getResourceAsStream("/null.avsc")).toString(true)
       schema.toString(true) shouldBe expected
     }
+
     "convert Arrays to List[X]" in {
       val json = """ { "foo" : [ true, false, true ] } """
       val schema = new JsonToAvroConverter("com.test.avro").convert("MyClass", json)
       val expected = new org.apache.avro.Schema.Parser().parse(getClass.getResourceAsStream("/arraysbooleans.avsc")).toString(true)
       schema.toString(true) shouldBe expected
+    }
+
+    "convert snakeCase json to camelCase schema" in {
+      val json = Source.fromInputStream(getClass.getResourceAsStream(s"/json1_with_snake_case.json")).getLines.mkString("\n")
+      val schema = new JsonToAvroConverter("com.test.avro", jsonNamingStrategy = SnakeCase).convert("MyClass", json)
+      schema.toString(true) shouldBe new org.apache.avro.Schema.Parser().parse(getClass.getResourceAsStream(s"/avro1.avsc")).toString(true)
+
+    }
+
+    "convert lispCase json to camelCase schema" in {
+
+      val json = Source.fromInputStream(getClass.getResourceAsStream(s"/json1_with_lisp_case.json")).getLines.mkString("\n")
+      val schema = new JsonToAvroConverter("com.test.avro", jsonNamingStrategy = LispCase).convert("MyClass", json)
+      schema.toString(true) shouldBe new org.apache.avro.Schema.Parser().parse(getClass.getResourceAsStream(s"/avro1.avsc")).toString(true)
+
+    }
+
+    "convert pascalCase json to camelCase schema" in {
+      for (k <- 1 to 2) {
+        val json = Source.fromInputStream(getClass.getResourceAsStream(s"/json${k}_with_pascal_case.json")).getLines.mkString("\n")
+        val schema = new JsonToAvroConverter("com.test.avro", jsonNamingStrategy = PascalCase).convert("MyClass", json)
+        schema.toString(true) shouldBe new org.apache.avro.Schema.Parser().parse(getClass.getResourceAsStream(s"/avro${k}.avsc")).toString(true)
+      }
     }
   }
 }
