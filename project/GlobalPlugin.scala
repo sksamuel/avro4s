8fc92cbd1fd1f3c1c0e9c023b0aee5ba74f74cba
andrew <uandrew1965@gmail.com>
2017-09-12 06:18:04 +0300

    .

--- project/GlobalPlugin.scala
+++ project/GlobalPlugin.scala
@@ -8,31 +8,31 @@ object GlobalPlugin extends AutoPlugin {
 
   val org = "com.sksamuel.avro4s"
 
-  val AvroVersion = "1.8.1"
-  val Log4jVersion = "1.2.17"
-  val ScalatestVersion = "3.0.0"
-  val ScalaVersion = "2.12.1"
+  val AvroVersion = "1.8.2"
+  val ScalatestVersion = "3.0.3"
+  val ScalaVersion = "2.12.3"
   val Slf4jVersion = "1.7.12"
+  val Log4jVersion = "1.2.17"
 
   override def requires = ReleasePlugin
   override def trigger = allRequirements
   override def projectSettings = publishingSettings ++ Seq(
     organization := org,
     scalaVersion := ScalaVersion,
-    crossScalaVersions := Seq("2.11.8", "2.12.1"),
+    //crossScalaVersions := Seq("2.11.8", "2.12.1"),
     resolvers += Resolver.mavenLocal,
     parallelExecution in Test := false,
     scalacOptions := Seq("-unchecked", "-deprecation", "-encoding", "utf8", "-Ywarn-unused-import",
       "-Xfatal-warnings", "-feature", "-language:existentials"
     ),
-    javacOptions := Seq("-source", "1.7", "-target", "1.7"),
+    javacOptions := Seq("-source", "1.8", "-target", "1.8"),
     libraryDependencies ++= Seq(
       "org.scala-lang"    % "scala-reflect"     % scalaVersion.value,
       "org.apache.avro"   % "avro"              % AvroVersion,
       "org.slf4j"         % "slf4j-api"         % Slf4jVersion,
-      "log4j"             % "log4j"             % Log4jVersion          % "test",
-      "org.slf4j"         % "log4j-over-slf4j"  % Slf4jVersion          % "test",
-      "org.scalatest"     %% "scalatest"        % ScalatestVersion      % "test"
+      "log4j"             % "log4j"             % Log4jVersion          % Test,
+      "org.slf4j"         % "log4j-over-slf4j"  % Slf4jVersion          % Test,
+      "org.scalatest"     %% "scalatest"        % ScalatestVersion      % Test
     )
   )
 
@@ -48,27 +48,6 @@ object GlobalPlugin extends AutoPlugin {
       } else {
         Some("releases" at s"${nexus}service/local/staging/deploy/maven2")
       }
-    },
-    pomExtra := {
-      <url>https://github.com/sksamuel/avro4s</url>
-        <licenses>
-          <license>
-            <name>MIT</name>
-            <url>https://opensource.org/licenses/MIT</url>
-            <distribution>repo</distribution>
-          </license>
-        </licenses>
-        <scm>
-          <url>git@github.com:sksamuel/avro4s.git</url>
-          <connection>scm:git@github.com:sksamuel/avro4s.git</connection>
-        </scm>
-        <developers>
-          <developer>
-            <id>sksamuel</id>
-            <name>sksamuel</name>
-            <url>http://github.com/sksamuel</url>
-          </developer>
-        </developers>
     }
   )
 }
