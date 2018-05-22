e63c4ca799eb517e39569ec7cd36119930fb9a65
Antonio Murgia <antonio.murgia@agilelab.it>
2018-05-22 09:36:13 +0200

    #200 Cleanup tests

--- avro4s-core/src/test/scala/com/sksamuel/avro4s/GenericClassTest.scala
+++ avro4s-core/src/test/scala/com/sksamuel/avro4s/GenericClassTest.scala
@@ -2,13 +2,41 @@ package com.sksamuel.avro4s
 
 import java.io.ByteArrayOutputStream
 
+import org.apache.avro.Schema
 import org.scalatest.FunSuite
 
+import scala.io.Source
+
 class GenericClassTest extends FunSuite {
-  test("Generic classes with different concrete types generate different type names") {
-    val bos = new ByteArrayOutputStream()
-    AvroOutputStream.data[MyData](bos).write(MyData(MyWrapper(1), Some(MyWrapper(""))))
-    assert(AvroOutputStream.data[MyData](bos).schema.toString() ===
-      """{"type":"record","name":"MyData","namespace":"com.sksamuel.avro4s","fields":[{"name":"i","type":{"type":"record","name":"MyWrapper_Int","fields":[{"name":"a","type":"int"}]}},{"name":"s","type":["null",{"type":"record","name":"MyWrapper_String","fields":[{"name":"a","type":"string"}]}]}]}""")
+  test("Generic classes with different concrete types generate different type names with default streams") {
+    val data = MyData(MyWrapper(1), Some(MyWrapper("")))
+    val byteArrayOutputStream = new ByteArrayOutputStream()
+    val outputStream = AvroOutputStream.data[MyData](byteArrayOutputStream)
+    outputStream.write(data)
+    outputStream.close()
+    byteArrayOutputStream.close()
+
+    val inputStream = AvroInputStream.data[MyData](byteArrayOutputStream.toByteArray)
+    assert(inputStream.iterator.next() === data)
+    assert(inputStream.datumReader.getSchema === expectedSchema)
+    assert(outputStream.schema === expectedSchema)
   }
+
+  test("Generic classes with different concrete types generate different type names with binary streams") {
+    val data = MyData(MyWrapper(1), Some(MyWrapper("")))
+    val byteArrayOutputStream = new ByteArrayOutputStream()
+    val outputStream = AvroOutputStream.binary[MyData](byteArrayOutputStream)
+    outputStream.write(data)
+    outputStream.close()
+    byteArrayOutputStream.close()
+
+    val inputStream = AvroInputStream.binary[MyData](byteArrayOutputStream.toByteArray)
+    assert(inputStream.iterator.next() === data)
+  }
+
+  val expectedSchema: Schema =
+    new Schema.Parser().parse(
+      Source.fromFile(getClass.getClassLoader.getResource("genericSchema.avsc").getFile).getLines().next()
+    )
+
 }
