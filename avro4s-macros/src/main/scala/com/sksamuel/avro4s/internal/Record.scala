b57088d83619db3596aff4f38a8693f54c90312a
sksamuel <sam@sksamuel.com>
2018-08-16 02:12:58 +0100

    Added RecordEncoder stub

--- avro4s-macros/src/main/scala/com/sksamuel/avro4s/internal/Record.scala
+++ avro4s-macros/src/main/scala/com/sksamuel/avro4s/internal/Record.scala
@@ -6,22 +6,22 @@ import org.apache.avro.{AvroRuntimeException, Schema}
 
 class Record(val schema: Schema) extends GenericRecord with SpecificRecord {
 
-  private val values = Array.ofDim[Any](schema.getFields.size)
+  private val values = Array.ofDim[AnyRef](schema.getFields.size)
 
-  override def put(key: String, value: Any): Unit = {
+  override def put(key: String, value: AnyRef): Unit = {
     val field = schema.getField(key)
     if (field == null) throw new AvroRuntimeException("Not a valid schema field: " + key)
     values.update(field.pos, value)
   }
 
-  override def get(key: String): Any = {
+  override def get(key: String): AnyRef = {
     val field = schema.getField(key)
     if (field == null) null else values(field.pos)
   }
 
-  override def put(i: Int, v: Any): Unit = values.update(i, v)
+  override def put(i: Int, v: AnyRef): Unit = values.update(i, v)
 
-  override def get(i: Int): Any = values(i)
+  override def get(i: Int): AnyRef = values(i)
 
   override def getSchema: Schema = schema
 }
---
+++ avro4s-macros/src/main/scala/com/sksamuel/avro4s/internal/RecordEncoder.scala
@@ -0,0 +1,28 @@
+package com.sksamuel.avro4s.internal
+
+import scala.language.experimental.macros
+
+trait RecordEncoder[T] {
+  def encode(t: T): Record
+}
+
+object RecordEncoder {
+
+  implicit def apply[T]: RecordEncoder[T] = macro applyImpl[T]
+
+  def applyImpl[T: c.WeakTypeTag](c: scala.reflect.macros.whitebox.Context): c.Expr[RecordEncoder[T]] = {
+
+    import c.universe._
+
+    val reflect = ReflectHelper(c)
+    val tpe = weakTypeTag[T].tpe
+
+    c.Expr[RecordEncoder[T]](
+      q"""
+          new _root_.com.sksamuel.avro4s.internal.RecordEncoder[$tpe] {
+            override def encode(t: $tpe): Record = null
+          }
+       """
+    )
+  }
+}
