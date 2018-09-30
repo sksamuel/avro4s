7f0dd1d5ea00528561684518ff6af465e8670816
sksamuel <sam@sksamuel.com>
2018-09-30 14:27:05 +0100

    Added test for @AvroField value type

--- avro4s-core/src/test/scala/com/sksamuel/avro4s/schema/AvroFixedAnnotationSchemaTest.scala
+++ avro4s-core/src/test/scala/com/sksamuel/avro4s/schema/AvroFixedAnnotationSchemaTest.scala
@@ -8,20 +8,31 @@ import org.scalatest.{Matchers, WordSpec}
 class AvroFixedSchemaTest extends WordSpec with Matchers {
 
   "@AvroFixed" should {
+
     "generate fixed schema when used on top level class" in {
       val schema = AvroSchema[FixedValueClass]
       schema.getType shouldBe Type.FIXED
       schema.getFixedSize shouldBe 8
+      schema.getFullName shouldBe "com.sksamuel.avro4s.schema.FixedValueClass"
     }
+
     "generated fixed field schema when used on a field" in {
       val schema = AvroSchema[FixedString]
       val expected = new org.apache.avro.Schema.Parser().parse(getClass.getResourceAsStream("/fixed_string.json"))
+      schema.toString(true) shouldBe expected.toString(true)
+    }
+
+    "generated fixed field schema when an annotated value type is used in a field" in {
+      val schema = AvroSchema[FooWithValue]
+      val expected = new org.apache.avro.Schema.Parser().parse(getClass.getResourceAsStream("/fixed_string_as_field.json"))
       schema.toString(true) shouldBe expected.toString(true)
     }
   }
 }
 
 case class FixedString(@AvroFixed(7) mystring: String)
+
+case class FooWithValue(z: FixedValueClass)
 
 @AvroFixed(8)
 case class FixedValueClass(bytes: Array[Byte]) extends AnyVal
