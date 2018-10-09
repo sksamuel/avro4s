e6ae5b3aeb4c1cddd50a0a4b16f23d0cde99be4f
Alex Good <alexjsgood@gmail.com>
2018-10-09 23:18:47 +0100

    Respect @AvroName in sealed case object enums

--- avro4s-macros/src/main/scala/com/sksamuel/avro4s/Decoder.scala
+++ avro4s-macros/src/main/scala/com/sksamuel/avro4s/Decoder.scala
@@ -289,8 +289,19 @@ object Decoder extends CoproductDecoders with TupleDecoders {
                                                                      toList: ToList[L, T]): Decoder[T] = new Decoder[T] {
     override def decode(value: Any, schema: Schema): T = {
       val name = value.toString
-      toList(objs()).find(_.toString == name).getOrElse(sys.error(s"Uknown type $name"))
+      val variants = toList(objs())
+      variants.find(v => {
+        println(getTypeName(v.getClass))
+        getTypeName(v.getClass) == name
+      }).getOrElse(sys.error(s"Unknown type $name"))
     }
+
+    private def getTypeName[T](clazz: Class[T]): String = {
+      val mirror = runtimeMirror(clazz.getClassLoader)
+      val tpe = mirror.classSymbol(clazz).toType
+      AvroNameResolver.forClass(tpe).asInstanceOf[String]
+    }
+
   }
 
   implicit def applyMacro[T <: Product]: Decoder[T] = macro applyMacroImpl[T]
