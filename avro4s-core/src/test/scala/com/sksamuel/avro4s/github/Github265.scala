a1b890ba9147ff5e5a16ab53e4f97c9fe5879d10
sksamuel <sam@sksamuel.com>
2019-01-28 20:24:13 +0000

    Added test for #265

---
+++ avro4s-core/src/test/scala/com/sksamuel/avro4s/github/Github265.scala
@@ -0,0 +1,19 @@
+package com.sksamuel.avro4s.github
+
+import com.sksamuel.avro4s.{Decoder, Encoder, RecordFormat, SchemaFor}
+import org.apache.avro.generic.GenericRecord
+import org.scalatest.{FunSuite, Matchers}
+
+class Github265 extends FunSuite with Matchers {
+
+  case class Ingredient(name: String, sugar: Double, fat: Double)
+  case class Pizza(name: String, ingredients: Seq[Ingredient], vegetarian: Boolean, vegan: Boolean, calories: Int)
+
+  val pepperoni = Pizza("pepperoni", Seq(Ingredient("pepperoni", 12, 4.4), Ingredient("onions", 1, 0.4)), false, false, 98)
+  val hawaiian = Pizza("hawaiian", Seq(Ingredient("ham", 1.5, 5.6), Ingredient("pineapple", 5.2, 0.2)), false, false, 91)
+
+  def toAvro[T: Encoder : Decoder : SchemaFor](obj: T): GenericRecord = RecordFormat[T].to(obj)
+
+  val recordInAvro = toAvro[Pizza](pepperoni)
+  printf(s"Avro of Pepperoni: $recordInAvro\n")
+}
