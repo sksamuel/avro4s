04935c2838dd8234c4d7de4b1456c2d6f0826afb
ChengGit <cheng.bo.ucd@gmail.com>
2019-03-11 17:37:31 +0000

    RecordFormat.from accepts IndexRecord Applied suggested change by update the parameter type from GenericRecord to IndexedRecord, in order to handle conversion between a specific record and case class

--- avro4s-core/src/main/scala/com/sksamuel/avro4s/FromRecord.scala
+++ avro4s-core/src/main/scala/com/sksamuel/avro4s/FromRecord.scala
@@ -1,18 +1,18 @@
 package com.sksamuel.avro4s
 
 import org.apache.avro.Schema
-import org.apache.avro.generic.GenericRecord
+import org.apache.avro.generic.IndexedRecord
 
 /**
   * Converts from an Avro GenericRecord into instances of T.
   */
 trait FromRecord[T] extends Serializable {
-  def from(record: GenericRecord): T
+  def from(record: IndexedRecord): T
 }
 
 object FromRecord {
   def apply[T: Decoder : SchemaFor]: FromRecord[T] = apply(AvroSchema[T])
   def apply[T: Decoder](schema: Schema): FromRecord[T] = new FromRecord[T] {
-    override def from(record: GenericRecord): T = implicitly[Decoder[T]].decode(record, record.getSchema)
+    override def from(record: IndexedRecord): T = implicitly[Decoder[T]].decode(record, record.getSchema)
   }
 }
