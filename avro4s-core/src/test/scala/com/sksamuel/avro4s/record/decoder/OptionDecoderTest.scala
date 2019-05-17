d7af2130517140eb477830dde5ede566f375ff4e
Ivan Luzyanin <ivan@acorns.com>
2019-05-17 11:37:42 -0700

    Enable decode non-optional avro fields with optionalDecoder

--- avro4s-core/src/test/scala/com/sksamuel/avro4s/record/decoder/OptionDecoderTest.scala
+++ avro4s-core/src/test/scala/com/sksamuel/avro4s/record/decoder/OptionDecoderTest.scala
@@ -7,12 +7,24 @@ import org.scalatest.{Matchers, WordSpec}
 
 case class OptionBoolean(b: Option[Boolean])
 case class OptionString(s: Option[String])
+case class RequiredString(s: String)
 
 class OptionDecoderTest extends WordSpec with Matchers {
 
   "Decoder" should {
     "support String options" in {
       val schema = AvroSchema[OptionString]
+
+      val record1 = new GenericData.Record(schema)
+      record1.put("s", "hello")
+      Decoder[OptionString].decode(record1, schema) shouldBe OptionString(Some("hello"))
+
+      val record2 = new GenericData.Record(schema)
+      record2.put("s", null)
+      Decoder[OptionString].decode(record2, schema) shouldBe OptionString(None)
+    }
+    "support decoding required fields as Option" in {
+      val schema = AvroSchema[RequiredString]
 
       val record1 = new GenericData.Record(schema)
       record1.put("s", "hello")
