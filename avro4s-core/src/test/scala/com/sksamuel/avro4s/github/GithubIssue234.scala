26ece7892b52be3970d8b76a0da99e3a24e45791
sksamuel <sam@sksamuel.com>
2019-05-17 23:57:48 -0500

    Restored some github tests

--- avro4s-core/src/test/scala/com/sksamuel/avro4s/github/GithubIssue234.scala
+++ avro4s-core/src/test/scala/com/sksamuel/avro4s/github/GithubIssue234.scala
@@ -1,16 +1,16 @@
-//package com.sksamuel.avro4s.github
-//
-//import com.sksamuel.avro4s._
-//import org.scalatest.{FunSuite, Matchers}
-//
-//case class TestEntry(name: String)
-//sealed trait TestClass // class or trait
-//final case class Test(id: Int, entries: List[TestEntry]) extends TestClass // <-- list
-//case class ContainsTestClass(testClass: TestClass)
-//
-//class GithubIssue234 extends FunSuite with Matchers {
-//
-//  test("RecordFormat macro for List - diverging implicit expansion for type #234") {
-//    val format: RecordFormat[ContainsTestClass] = RecordFormat[ContainsTestClass]
-//  }
-//}
+package com.sksamuel.avro4s.github
+
+import com.sksamuel.avro4s._
+import org.scalatest.{FunSuite, Matchers}
+
+case class TestEntry(name: String)
+sealed trait TestClass // class or trait
+final case class Test(id: Int, entries: List[TestEntry]) extends TestClass // <-- list
+case class ContainsTestClass(testClass: TestClass)
+
+class GithubIssue234 extends FunSuite with Matchers {
+
+  test("RecordFormat macro for List - diverging implicit expansion for type #234") {
+    val format: RecordFormat[ContainsTestClass] = RecordFormat[ContainsTestClass]
+  }
+}
