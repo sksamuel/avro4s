ffdaba6356fc6abc84372156d684d0f6e912e94a
sksamuel <sam@sksamuel.com>
2019-05-19 18:39:21 -0500

    Restored test for github 187

--- avro4s-core/src/test/scala/com/sksamuel/avro4s/github/GithubIssue187And152.scala
+++ avro4s-core/src/test/scala/com/sksamuel/avro4s/github/GithubIssue187And152.scala
@@ -1,28 +1,33 @@
 package com.sksamuel.avro4s.github
 
+import java.io.File
+
+import com.sksamuel.avro4s.{AvroInputStream, AvroOutputStream, AvroSchema, Encoder}
 import org.scalatest.{FunSuite, Matchers}
 
 class GithubIssue187And152 extends FunSuite with Matchers {
 
+  import GithubIssue187And152._
+
   test("Encoding and decoding a nested ADT") {
 
-   // val schema = AvroSchema[Product1]
-   // val expected = new org.apache.avro.Schema.Parser().parse(getClass.getResourceAsStream("/github187.json"))
-   // schema.toString(true) shouldBe expected.toString(true)
+    val schema = AvroSchema[Product1]
+    val expected = new org.apache.avro.Schema.Parser().parse(getClass.getResourceAsStream("/github187.json"))
+    schema.toString(true) shouldBe expected.toString(true)
 
-    //  val data = Product1(5, Product1.Coproduct1.Coproduct1Summand2(7, Product1.Coproduct1.Coproduct1Summand2.Coproduct2.One))
+    val data = Product1(5, Product1.Coproduct1.Coproduct1Summand2(7, Product1.Coproduct1.Coproduct1Summand2.Coproduct2.One))
 
-    // val encoder = Encoder[Product1]
+    val encoder = Encoder[Product1]
 
-    //    val file: File = new File("product1.avro")
-    //    val os = AvroOutputStream.binary[Product1](file, schema)
-    //    os.write(data)
-    //    os.close()
-    //
-    //    val is = AvroInputStream.binary[Product1](file, schema)
-    //    val decoded = is.iterator.toList
-    //
-    //    decoded shouldEqual List(data)
+    val file: File = new File("product1.avro")
+    val os = AvroOutputStream.binary[Product1].to(file).build(schema)
+    os.write(data)
+    os.close()
+
+    val is = AvroInputStream.binary[Product1].from(file).build(schema)
+    val decoded = is.iterator.toList
+
+    decoded shouldEqual List(data)
   }
 }
 
@@ -41,10 +46,10 @@ object GithubIssue187And152 {
         sealed trait Coproduct2
 
         object Coproduct2 {
-          // final case object One extends Coproduct2
-          //  final case object Two extends Coproduct2
+          final case object One extends Coproduct2
+          final case object Two extends Coproduct2
           final case class Three(s: String) extends Coproduct2
-          //  final case class Four() extends Coproduct2
+          final case class Four() extends Coproduct2
         }
       }
 
