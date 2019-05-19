10346d2d38e4c6cd93dba790b8f5cff8a6056241
sksamuel <sam@sksamuel.com>
2019-05-18 23:20:45 -0500

    Fixed compile error

--- avro4s-cats/src/main/scala/com/sksamuel/avro4s/cats/Decoders.scala
+++ avro4s-cats/src/main/scala/com/sksamuel/avro4s/cats/Decoders.scala
@@ -1,7 +1,7 @@
 package com.sksamuel.avro4s.cats
 
 import cats.data.{NonEmptyList, NonEmptyVector}
-import com.sksamuel.avro4s.Decoder
+import com.sksamuel.avro4s.{Decoder, DefaultNamingStrategy, NamingStrategy}
 import org.apache.avro.Schema
 
 import scala.language.implicitConversions
@@ -11,7 +11,7 @@ object Decoders {
   import scala.collection.JavaConverters._
 
   implicit def nonEmptyListEncoder[T](decoder: Decoder[T]) = new Decoder[NonEmptyList[T]] {
-    override def decode(value: Any, schema: Schema): NonEmptyList[T] = value match {
+    override def decode(value: Any, schema: Schema)(implicit naming: NamingStrategy = DefaultNamingStrategy): NonEmptyList[T] = value match {
       case array: Array[_] => NonEmptyList.fromListUnsafe(array.toList.map(decoder.decode(_, schema)))
       case list: java.util.Collection[_] => NonEmptyList.fromListUnsafe(list.asScala.map(decoder.decode(_, schema)).toList)
       case other => sys.error("Unsupported type " + other)
@@ -19,7 +19,7 @@ object Decoders {
   }
 
   implicit def nonEmptyVectorEncoder[T](decoder: Decoder[T]) = new Decoder[NonEmptyVector[T]] {
-    override def decode(value: Any, schema: Schema): NonEmptyVector[T] = value match {
+    override def decode(value: Any, schema: Schema)(implicit naming: NamingStrategy = DefaultNamingStrategy): NonEmptyVector[T] = value match {
       case array: Array[_] => NonEmptyVector.fromVectorUnsafe(array.toVector.map(decoder.decode(_, schema)))
       case list: java.util.Collection[_] => NonEmptyVector.fromVectorUnsafe(list.asScala.map(decoder.decode(_, schema)).toVector)
       case other => sys.error("Unsupported type " + other)
