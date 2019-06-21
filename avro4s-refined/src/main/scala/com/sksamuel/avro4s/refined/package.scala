72da4ec0af0f282c0cc91361a2ad38f3246ca660
Lawrence Carvalho <lacarvalho91@gmail.com>
2019-06-21 13:46:55 +0100

    didn't realise all these typeclasses have summoners, switch implicit params to context bounds where possible

--- avro4s-refined/src/main/scala/com/sksamuel/avro4s/refined/package.scala
+++ avro4s-refined/src/main/scala/com/sksamuel/avro4s/refined/package.scala
@@ -4,15 +4,12 @@ import eu.timepit.refined.api.{RefType, Validate}
 
 package object refined {
 
-  implicit def refinedSchemaFor[T, P, F[_, _]](implicit schemaFor: SchemaFor[T]): SchemaFor[F[T, P]] =
-    (namingStrategy: NamingStrategy) => schemaFor.schema(namingStrategy)
+  implicit def refinedSchemaFor[T: SchemaFor, P, F[_, _]]: SchemaFor[F[T, P]] =
+    (namingStrategy: NamingStrategy) => SchemaFor[T].schema(namingStrategy)
 
-  implicit def refinedEncoder[T, P, F[_, _]](implicit encoder: Encoder[T],
-                                             refType: RefType[F]): Encoder[F[T, P]] =
-    encoder.comap(refType.unwrap)
+  implicit def refinedEncoder[T: Encoder, P, F[_, _] : RefType]: Encoder[F[T, P]] =
+    Encoder[T].comap(RefType[F].unwrap)
 
-  implicit def refinedDecoder[T, P, F[_, _]](implicit decoder: Decoder[T],
-                                             validate: Validate[T, P],
-                                             refType: RefType[F]): Decoder[F[T, P]] =
-    decoder.map(refType.refine[P].unsafeFrom[T])
+  implicit def refinedDecoder[T: Decoder, P, F[_, _] : RefType](implicit validate: Validate[T, P]): Decoder[F[T, P]] =
+    Decoder[T].map(RefType[F].refine[P].unsafeFrom[T])
 }
