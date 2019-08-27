780fcb9b554a2566ceb86881d563f8eb6cf78918
bobbyrauchenberg <bobbyrauchenberg@gmail.com>
2019-08-27 13:40:53 +0100

    Fixing Option default bug

--- avro4s-core/src/test/scala/com/sksamuel/avro4s/record/decoder/OptionDecoderTest.scala
+++ avro4s-core/src/test/scala/com/sksamuel/avro4s/record/decoder/OptionDecoderTest.scala
@@ -1,7 +1,7 @@
 package com.sksamuel.avro4s.record.decoder
 
-import com.sksamuel.avro4s.{AvroSchema, Decoder, DefaultFieldMapper}
-import org.apache.avro.generic.GenericData
+import com.sksamuel.avro4s.{AvroSchema, Decoder, DefaultFieldMapper, ToRecord}
+import org.apache.avro.generic.{GenericData, GenericRecordBuilder}
 import org.scalatest.{Matchers, WordSpec}
 
 case class OptionBoolean(b: Option[Boolean])
@@ -15,6 +15,11 @@ case object SnoutleyOptionEnum extends CupcatOptionEnum
 
 case class OptionStringDefault(s: Option[String] = Option("cupcat"))
 case class OptionEnumDefault(s: Option[CupcatOptionEnum] = Option(CuppersOptionEnum))
+
+case class OptionEnumDefaultWithNone(s: Option[CupcatOptionEnum] = Option(CuppersOptionEnum), t: String)
+case class OptionStringDefaultWithNone(s: Option[String] = Option("cupcat"), t: String)
+
+case class SchemaWithoutExpectedField(t: String)
 
 class OptionDecoderTest extends WordSpec with Matchers {
 
@@ -48,18 +53,29 @@ class OptionDecoderTest extends WordSpec with Matchers {
       record2.put("b", null)
       Decoder[OptionBoolean].decode(record2, schema, DefaultFieldMapper) shouldBe OptionBoolean(None)
     }
-    "decode an option whose default contains a string" in {
+    "if a field is missing, use default value" in {
       val schema = AvroSchema[OptionStringDefault]
 
-      val record1 = new GenericData.Record(schema)
+      val record1 = new GenericData.Record(AvroSchema[SchemaWithoutExpectedField])
+
       Decoder[OptionStringDefault].decode(record1, schema, DefaultFieldMapper) shouldBe OptionStringDefault(Some("cupcat"))
     }
-    "decode an option whose default contains an enum" in {
+    "if an enum field is missing, use default value" in {
       val schema = AvroSchema[OptionEnumDefault]
 
-      val record1 = new GenericData.Record(schema)
+      val record1 = new GenericData.Record(AvroSchema[SchemaWithoutExpectedField])
       Decoder[OptionEnumDefault].decode(record1, schema, DefaultFieldMapper) shouldBe OptionEnumDefault(Some(CuppersOptionEnum))
     }
+    "decode a null field to None" in {
+      val schema = AvroSchema[OptionEnumDefaultWithNone]
+
+      val record1 = new GenericData.Record(schema)
+      record1.put("s", null)
+      record1.put("t", "cupcat")
+
+      Decoder[OptionEnumDefaultWithNone].decode(record1, schema, DefaultFieldMapper) shouldBe OptionEnumDefaultWithNone(None, "cupcat")
+    }
+
   }
 }
 
