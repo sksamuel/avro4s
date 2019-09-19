7423edfa510e73435e513c7519fcd176f359a401
bobbyrauchenberg <bobbyrauchenberg@gmail.com>
2019-09-19 15:56:38 +0100

    fixes broken test for issue 202

--- avro4s-core/src/test/scala/com/sksamuel/avro4s/github/Github202.scala
+++ avro4s-core/src/test/scala/com/sksamuel/avro4s/github/Github202.scala
@@ -3,18 +3,17 @@ package com.sksamuel.avro4s.github
 import java.io.ByteArrayOutputStream
 
 import com.sksamuel.avro4s.{AvroAlias, AvroInputStream, AvroOutputStream, AvroSchema}
+import org.apache.avro.generic.GenericData
 import org.scalatest.{FunSuite, Matchers}
 
 class Github202 extends FunSuite with Matchers {
 
-  ignore("Alias avro schema feature problem #202") {
+  test("Alias avro schema feature problem #202") {
 
     case class Version1(a: String)
 
+    @AvroAlias("Version1")
     case class Version2(@AvroAlias("a") renamed: String, `new`: String = "default")
-
-    println(AvroSchema[Version1].toString(true))
-    println(AvroSchema[Version2].toString(true))
 
     val v1 = Version1("hello")
     val baos = new ByteArrayOutputStream()
@@ -22,13 +21,76 @@ class Github202 extends FunSuite with Matchers {
     output.write(v1)
     output.close()
 
-    println(baos.toByteArray.toList)
-
     val is = AvroInputStream.data[Version2].from(baos.toByteArray).build(AvroSchema[Version1], AvroSchema[Version2])
     val v2 = is.iterator.toList.head
     is.close()
 
     assert(v2.renamed == v1.a)
     assert(v2.`new` == "default")
+  }
+
+
+
+  test("Alias avro schema feature problem #202 using java with alias") {
+
+    val writeSchema =
+      """
+        |{"namespace": "com.sksamuel.avro4s.github.Github202", "type": "record", "name": "ClubToRead",
+        |    "fields": [
+        |        {"name": "full_name", "type": "string"},
+        |        {"name": "foundation_year", "type": "int"}
+        |    ]
+        |}
+      """.stripMargin.replace("\n", "")
+
+    val readSchema =
+      """
+        |{"namespace": "com.sksamuel.avro4s.github.Github202", "type": "record", "name": "ClubToRead", "aliases":["ClubToRead"],
+        |"fields": [
+        |        {"name": "fullName", "type": "string", "aliases": ["full_name"]},
+        |        {"name": "foundationYear", "type": "int", "aliases": ["foundation_year"]}
+        |    ]
+        |}
+      """.stripMargin.replace("\n", "")
+
+    import java.io.ByteArrayOutputStream
+
+    import org.apache.avro.file.{DataFileReader, DataFileWriter, SeekableByteArrayInput}
+    import org.apache.avro.generic.{GenericDatumReader, GenericDatumWriter, GenericRecord}
+
+    import collection.JavaConverters._
+
+    val writerSchema = new org.apache.avro.Schema.Parser().parse(writeSchema)
+    val readerSchema = new org.apache.avro.Schema.Parser().parse(readSchema)
+
+    val recordV1 = new GenericData.Record(writerSchema)
+    recordV1.put("full_name", "cupcat")
+    recordV1.put("foundation_year", 5)
+
+    val baos = new ByteArrayOutputStream()
+    val datumWriter = new GenericDatumWriter[GenericRecord](writerSchema)
+    val dataFileWriter = new DataFileWriter[GenericRecord](datumWriter)
+    dataFileWriter.create(writerSchema, baos)
+
+    dataFileWriter.append(recordV1)
+
+    dataFileWriter.flush()
+    dataFileWriter.close()
+
+    baos.toByteArray
+
+
+    val datumReader = new GenericDatumReader[GenericRecord](readerSchema)
+    val inputStream = new SeekableByteArrayInput(baos.toByteArray)
+    val dataFileReader = new DataFileReader[GenericRecord](inputStream, datumReader)
+
+    val dataFromAvro = dataFileReader.iterator().asScala.toList
+
+    dataFileReader.close()
+
+
+    dataFromAvro.head.get("fullName").toString shouldBe "cupcat"
+    dataFromAvro.head.get("foundationYear").toString.toInt shouldBe 5
+
   }
 }
