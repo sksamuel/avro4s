bd0b7f2498970aecac0e3dd1f6f8e074e6c6721c
ikryvorotenko <ikryvorotenko@gmail.com>
2019-10-04 15:50:14 -0500

    Fixed serializing GenericRecord using AvroDataOutputStream

--- avro4s-core/src/test/scala/com/sksamuel/avro4s/streams/output/AvroDataOutputStreamCodecTest.scala
+++ avro4s-core/src/test/scala/com/sksamuel/avro4s/streams/output/AvroDataOutputStreamCodecTest.scala
@@ -1,9 +1,10 @@
 package com.sksamuel.avro4s.streams.output
 
-import java.io.ByteArrayOutputStream
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
 
-import com.sksamuel.avro4s.{AvroOutputStream, AvroSchema}
-import org.apache.avro.file.CodecFactory
+import com.sksamuel.avro4s.{AvroOutputStream, AvroSchema, Encoder}
+import org.apache.avro.file.{CodecFactory, DataFileStream}
+import org.apache.avro.generic.{GenericDatumReader, GenericRecord, GenericRecordBuilder}
 import org.scalatest.{Matchers, WordSpec}
 
 class AvroDataOutputStreamCodecTest extends WordSpec with Matchers {
@@ -44,6 +45,28 @@ class AvroDataOutputStreamCodecTest extends WordSpec with Matchers {
       output.write(ennio)
       output.close()
       new String(baos.toByteArray) should include("bzip2")
+    }
+
+    "serialize generic record" in {
+      import scala.collection.JavaConverters._
+      val record = new GenericRecordBuilder(schema)
+        .set("name", "ennio morricone")
+        .set("birthplace", "rome")
+        .set("compositions", List("legend of 1900", "ecstasy of gold").asJava)
+        .build()
+
+      implicit val encoder: Encoder[GenericRecord] = (r, _, _) => r
+
+      val baos = new ByteArrayOutputStream()
+      val output = AvroOutputStream.data[GenericRecord].to(baos).build(schema)
+      output.write(record)
+      output.close()
+
+      val bais = new ByteArrayInputStream(baos.toByteArray)
+      val reader = new DataFileStream[GenericRecord](bais, new GenericDatumReader[GenericRecord](schema))
+      val result = reader.iterator().asScala.toList
+      result should have size 1
+      result.head shouldBe record
     }
   }
 }
