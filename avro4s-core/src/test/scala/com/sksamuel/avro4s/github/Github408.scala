b7ab40117e3334c2b09e81870ad17a6168bda3b5
sksamuel <sam@sksamuel.com>
2019-12-31 00:07:16 -0600

    Added test for #408

--- avro4s-core/src/test/scala/com/sksamuel/avro4s/github/Github408.scala
+++ avro4s-core/src/test/scala/com/sksamuel/avro4s/github/Github408.scala
@@ -1,11 +1,13 @@
 package com.sksamuel.avro4s.github
 
-import org.scalatest.funsuite.AnyFunSuite
-import org.scalatest.matchers.must.Matchers
+import com.sksamuel.avro4s.streams.input.InputStreamTest
 
-class Github408 extends AnyFunSuite with Matchers {
+case class Woo(b: Boolean)
+case class OptionOfSeqOfCaseClass(ws: Option[Seq[Woo]])
 
-  test("deserialization of Option[Seq[CaseClass]]") {
+class Github408 extends InputStreamTest {
 
+  test("round trip of Option[Seq[CaseClass]]") {
+    writeRead(OptionOfSeqOfCaseClass(Some(List(Woo(true), Woo(false), Woo(true)))))
   }
 }
