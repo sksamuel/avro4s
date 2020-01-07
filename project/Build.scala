bdc0e4cc701f336c6e5726a873240919e8802911
Scala Steward <me@scala-steward.org>
2020-01-07 16:37:57 +0100

    Update magnolia to 0.12.6

--- project/Build.scala
+++ project/Build.scala
@@ -15,7 +15,7 @@ object Build extends AutoPlugin {
     val CatsVersion = "2.0.0-RC2"
     val ShapelessVersion = "2.3.3"
     val RefinedVersion = "0.9.10"
-    val MagnoliaVersion = "0.12.5"
+    val MagnoliaVersion = "0.12.6"
     val ScalaMeterVersion = "0.19"
   }
 
