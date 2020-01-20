5183c06acdc59384dc80fb2c7a2d6b59fd7a5f94
Wessel van Staal <w.vanstaal@eaglescience.nl>
2020-01-20 15:41:23 +0100

    support recursive types

--- avro4s-core/src/main/scala/com/sksamuel/avro4s/BigDecimals.scala
+++ avro4s-core/src/main/scala/com/sksamuel/avro4s/BigDecimals.scala
@@ -1,9 +1,10 @@
 package com.sksamuel.avro4s
 
+import magnolia.TypeName
 import org.apache.avro.{Schema, SchemaBuilder}
 
 object BigDecimals {
   implicit object AsString extends SchemaFor[BigDecimal] {
-    override def schema(fieldMapper: FieldMapper): Schema = SchemaBuilder.builder().stringType()
+    override def schema(fieldMapper: FieldMapper, parents: Map[Class[_], Schema]): Schema = SchemaBuilder.builder().stringType()
   }
 }
