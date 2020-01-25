9b8226755cfea85f65ff4e1adc980419a5740188
Andreas Schroeder <andreas.c.schroeder@gmail.com>
2020-01-25 21:33:13 +0100

    Convert EitherEncoderTest

--- avro4s-core/src/test/scala/com/sksamuel/avro4s/record/encoder/EitherEncoderTest.scala
+++ avro4s-core/src/test/scala/com/sksamuel/avro4s/record/encoder/EitherEncoderTest.scala
@@ -1,6 +1,6 @@
 package com.sksamuel.avro4s.record.encoder
 
-import com.sksamuel.avro4s.{AvroSchema, DefaultFieldMapper, Encoder, ImmutableRecord}
+import com.sksamuel.avro4s.{AvroSchemaV2, EncoderV2, ImmutableRecord}
 import org.apache.avro.util.Utf8
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
@@ -9,16 +9,16 @@ class EitherEncoderTest extends AnyFunSuite with Matchers {
 
   test("generate union:T,U for Either[T,U] of primitives") {
     case class Test(either: Either[String, Double])
-    Encoder[Test].encode(Test(Left("foo")), AvroSchema[Test], DefaultFieldMapper) shouldBe ImmutableRecord(AvroSchema[Test], Vector(new Utf8("foo")))
-    Encoder[Test].encode(Test(Right(234.4D)), AvroSchema[Test], DefaultFieldMapper) shouldBe ImmutableRecord(AvroSchema[Test], Vector(java.lang.Double.valueOf(234.4D)))
+    EncoderV2[Test].encode(Test(Left("foo"))) shouldBe ImmutableRecord(AvroSchemaV2[Test], Vector(new Utf8("foo")))
+    EncoderV2[Test].encode(Test(Right(234.4D))) shouldBe ImmutableRecord(AvroSchemaV2[Test], Vector(java.lang.Double.valueOf(234.4D)))
   }
 
   test("generate union:T,U for Either[T,U] of records") {
     case class Goo(s: String)
     case class Foo(b: Boolean)
     case class Test(either: Either[Goo, Foo])
-    Encoder[Test].encode(Test(Left(Goo("zzz"))), AvroSchema[Test], DefaultFieldMapper) shouldBe ImmutableRecord(AvroSchema[Test], Vector(ImmutableRecord(AvroSchema[Goo], Vector(new Utf8("zzz")))))
-    Encoder[Test].encode(Test(Right(Foo(true))), AvroSchema[Test], DefaultFieldMapper) shouldBe ImmutableRecord(AvroSchema[Test], Vector(ImmutableRecord(AvroSchema[Foo], Vector(java.lang.Boolean.valueOf(true)))))
+    EncoderV2[Test].encode(Test(Left(Goo("zzz")))) shouldBe ImmutableRecord(AvroSchemaV2[Test], Vector(ImmutableRecord(AvroSchemaV2[Goo], Vector(new Utf8("zzz")))))
+    EncoderV2[Test].encode(Test(Right(Foo(true)))) shouldBe ImmutableRecord(AvroSchemaV2[Test], Vector(ImmutableRecord(AvroSchemaV2[Foo], Vector(java.lang.Boolean.valueOf(true)))))
   }
 }
 
