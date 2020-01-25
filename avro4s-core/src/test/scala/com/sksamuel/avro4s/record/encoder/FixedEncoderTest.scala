30b669f36c4602d5812f29f14b5bd42ba57a12e1
Andreas Schroeder <andreas.c.schroeder@gmail.com>
2020-01-25 21:51:58 +0100

    Convert FixedEncoderTest

--- avro4s-core/src/test/scala/com/sksamuel/avro4s/record/encoder/FixedEncoderTest.scala
+++ avro4s-core/src/test/scala/com/sksamuel/avro4s/record/encoder/FixedEncoderTest.scala
@@ -1,6 +1,6 @@
 package com.sksamuel.avro4s.record.encoder
 
-import com.sksamuel.avro4s.{AvroFixed, AvroSchema, DefaultFieldMapper, Encoder}
+import com.sksamuel.avro4s.{AvroFixed, AvroSchema, DefaultFieldMapper, Encoder, EncoderV2, SchemaForV2}
 import org.apache.avro.generic.{GenericFixed, GenericRecord}
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
@@ -24,20 +24,20 @@ class FixedEncoderTest extends AnyFunSuite with Matchers {
   )
 
   test("encode fixed when used on a value type") {
-    val schema = AvroSchema[AvroMessage]
-    val record = Encoder[AvroMessage].encode(m, schema, DefaultFieldMapper).asInstanceOf[GenericRecord]
+    val schema = SchemaForV2[AvroMessage]
+    val record = EncoderV2[AvroMessage].encode(m).asInstanceOf[GenericRecord]
     record.get("q").asInstanceOf[GenericFixed].bytes().toVector shouldBe Vector(0, 1, 2, 3, 4, 5, 6, 0)
   }
 
   test("encode fixed when used on a field in a case class") {
-    val schema = AvroSchema[FixedString]
-    val record = Encoder[FixedString].encode(FixedString("sam"), schema, DefaultFieldMapper).asInstanceOf[GenericRecord]
+    val schema = SchemaForV2[FixedString]
+    val record = EncoderV2[FixedString].encode(FixedString("sam")).asInstanceOf[GenericRecord]
     record.get("mystring").asInstanceOf[GenericFixed].bytes.toVector shouldBe Vector(115, 97, 109, 0, 0, 0, 0)
   }
 
   test("support options of fixed") {
-    val schema = AvroSchema[OptionFixedWrapper]
-    val record = Encoder[OptionFixedWrapper].encode(OptionFixedWrapper(Some(FixedValueType("sam"))), schema, DefaultFieldMapper).asInstanceOf[GenericRecord]
+    val schema = SchemaForV2[OptionFixedWrapper]
+    val record = EncoderV2[OptionFixedWrapper].encode(OptionFixedWrapper(Some(FixedValueType("sam")))).asInstanceOf[GenericRecord]
     record.get("opt").asInstanceOf[GenericFixed].bytes.toVector shouldBe Vector(115, 97, 109, 0, 0, 0, 0, 0)
   }
 }
