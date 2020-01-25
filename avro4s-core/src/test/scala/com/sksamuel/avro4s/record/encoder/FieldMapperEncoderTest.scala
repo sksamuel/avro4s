bbab9efc306d9e6c7e43d87e2f5e9e4385ab80ad
Andreas Schroeder <andreas.c.schroeder@gmail.com>
2020-01-25 21:50:56 +0100

    Convert FieldMapperEncoderTest

--- avro4s-core/src/test/scala/com/sksamuel/avro4s/record/encoder/FieldMapperEncoderTest.scala
+++ avro4s-core/src/test/scala/com/sksamuel/avro4s/record/encoder/FieldMapperEncoderTest.scala
@@ -1,6 +1,6 @@
 package com.sksamuel.avro4s.record.encoder
 
-import com.sksamuel.avro4s.{Encoder, SchemaFor, SnakeCase}
+import com.sksamuel.avro4s.{EncoderV2, SchemaForV2, SnakeCase}
 import org.apache.avro.generic.GenericRecord
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
@@ -8,9 +8,10 @@ import org.scalatest.matchers.should.Matchers
 class FieldMapperEncoderTest extends AnyFunSuite with Matchers {
 
   test("adding an in scope FieldMapper should overide the fields in an encoder") {
-    val schema = SchemaFor[NamingTest].schema(SnakeCase)
-    val encoder = Encoder[NamingTest]
-    val record = encoder.encode(NamingTest("Foo"), schema, SnakeCase).asInstanceOf[GenericRecord]
+    implicit val fieldMapper = SnakeCase
+    val schema: SchemaForV2[NamingTest] = SchemaForV2[NamingTest]
+    val encoder = EncoderV2[NamingTest]
+    val record = encoder.encode(NamingTest("Foo")).asInstanceOf[GenericRecord]
     record.get("camel_case")
   }
 
