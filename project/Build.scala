767af7e3e93adb88a2a04049040014c4914039d4
sksamuel <sam@sksamuel.com>
2020-02-02 18:04:13 -0600

    Release 3.0.6

--- project/Build.scala
+++ project/Build.scala
@@ -71,7 +71,7 @@ object Build extends AutoPlugin {
     if (isTravis) {
       version := s"3.1.0.$travisBuildNumber-SNAPSHOT"
     } else {
-      version := "3.0.5"
+      version := "3.0.6"
     },
     publishTo := {
       val nexus = "https://oss.sonatype.org/"
