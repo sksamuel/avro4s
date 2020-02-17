55216d3d3005171ce49ba39de622f9d53b0d3a61
Chris Birchall <chris.birchall@47deg.com>
2020-02-17 19:19:41 +0000

    Support shapeless coproduct fields with default values
    Support for schema generation for fields with shapeless Coproduct type
    and default value.
    
    Note that we only support `Inl(...)`, not `Inr(...(Inl(...)))`, because
    "the type of the default value must match the first element of the
    union" (quoted from the Avro spec).
    
    Without the `Inl(x)` case added to `DefaultResolver`, the pattern match
    falls into the `Product` case and the test fails with:
    
    ```
    org.apache.avro.AvroTypeException: Invalid default for field u: {"head":123} not a ["int","string","boolean",{"type":"record","name":"Gimble","namespace":"com.sksamuel.avro4s.schema","fields":[{"name":"x","type":"string"}]}]
    ```

--- avro4s-core/src/test/scala/com/sksamuel/avro4s/schema/CoproductSchemaTest.scala
+++ avro4s-core/src/test/scala/com/sksamuel/avro4s/schema/CoproductSchemaTest.scala
@@ -1,7 +1,7 @@
 package com.sksamuel.avro4s.schema
 
 import com.sksamuel.avro4s.AvroSchema
-import shapeless.{:+:, CNil}
+import shapeless.{:+:, Coproduct, CNil}
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
@@ -24,11 +24,18 @@ class CoproductSchemaTest extends AnyFunSuite with Matchers {
     val expected = new org.apache.avro.Schema.Parser().parse(getClass.getResourceAsStream("/coproduct_of_coproducts.json"))
     coproductOfCoproducts.toString(true) shouldBe expected.toString(true)
   }
+
+  test("coproducts with default arguments") {
+    val schema = AvroSchema[CPWithDefault]
+    val expected = new org.apache.avro.Schema.Parser().parse(getClass.getResourceAsStream("/coproduct_with_default.json"))
+    schema.toString(true) shouldBe expected.toString(true)
+  }
 }
 
 case class Gimble(x: String)
 case class CPWrapper(u: CPWrapper.ISBG)
 case class CPWithOption(u: Option[CPWrapper.ISBG])
+case class CPWithDefault(u: CPWrapper.ISBG = Coproduct[CPWrapper.ISBG](123))
 object CPWrapper {
   type ISBG = Int :+: String :+: Boolean :+: Gimble :+: CNil
 }
