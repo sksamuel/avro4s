b3c828bd26e93f73ee159178bda005510896b31b
Andreas Schroeder <andreas.c.schroeder@gmail.com>
2020-04-30 11:53:06 +0200

    Pre-compute field name indices to reduce access time in ImmutableRecord.get

--- avro4s-core/src/main/scala/com/sksamuel/avro4s/ImmutableRecord.scala
+++ avro4s-core/src/main/scala/com/sksamuel/avro4s/ImmutableRecord.scala
@@ -11,17 +11,21 @@ import org.apache.avro.specific.SpecificRecord
 trait Record extends GenericRecord with SpecificRecord
 
 case class ImmutableRecord(schema: Schema, values: Seq[AnyRef]) extends Record {
-  require(schema.getType == Schema.Type.RECORD, "Cannot create an ImmutableRecord with a schema that is not a RECORD")
-
   import scala.collection.JavaConverters._
+
+  require(schema.getType == Schema.Type.RECORD, "Cannot create an ImmutableRecord with a schema that is not a RECORD")
+  require(schema.getFields.size == values.size,
+    s"Schema field size (${schema.getFields.size}) and value array size (${values.size}) must match")
+  require(schema.getFields.asScala.forall(_.pos < values.size),
+    s"Schema field position(s) (${schema.getFields.asScala.map(_.pos).filter(_ >= values.size).mkString(", ")}) must be within value array size (${values.size})")
+
+  private val indices: Map[String, Int] = schema.getFields.asScala.map(f => f.name -> f.pos).toMap
 
   override def put(key: String, v: scala.Any): Unit = throw new UnsupportedOperationException("This implementation of Record is immutable")
   override def put(i: Int, v: scala.Any): Unit = throw new UnsupportedOperationException("This implementation of Record is immutable")
 
   override def get(key: String): AnyRef = {
-    val index = schema.getFields.asScala.indexWhere(_.name == key)
-    if (index == -1)
-      sys.error(s"Field $key does not exist in this record (schema=$schema, values=$values)")
+    val index = indices.getOrElse(key, sys.error(s"Field $key does not exist in this record (schema=$schema, values=$values)"))
     get(index)
   }
 
