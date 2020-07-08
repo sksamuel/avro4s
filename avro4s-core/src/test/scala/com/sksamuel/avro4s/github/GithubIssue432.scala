4aabfa7af2dc0501d3235e2223527f96b4ba66a0
Seth Tisue <seth@tisue.net>
2020-07-08 18:58:44 -0500

    upgrade ScalaTest to version 3.2.0

--- avro4s-core/src/test/scala/com/sksamuel/avro4s/github/GithubIssue432.scala
+++ avro4s-core/src/test/scala/com/sksamuel/avro4s/github/GithubIssue432.scala
@@ -3,9 +3,9 @@ package com.sksamuel.avro4s.github
 import java.io.{ByteArrayInputStream, ByteArrayOutputStream, ObjectInputStream, ObjectOutputStream}
 
 import com.sksamuel.avro4s.Encoder
-import org.scalatest.{FunSuite, Matchers}
+import org.scalatest.funsuite.AnyFunSuite
 
-class GithubIssue432 extends FunSuite with Matchers {
+class GithubIssue432 extends AnyFunSuite {
 
   test("Serializable Encoder[BigDecimal] #432") {
     val oos = new ObjectOutputStream(new ByteArrayOutputStream())
