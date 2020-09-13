baf372e05ab8e63ff6e7fe5cb68dbe23a5ba48fc
Sebastian Ziegler <sebastian.ziegler.m@gmail.com>
2020-09-13 08:41:49 +0200

    Change type guard order. Fixes #545 (#546)
    * Change guard order
    
    * Change guard order

---
+++ avro4s-core/src/test/scala/com/sksamuel/avro4s/github/GithubIssue545.scala
@@ -0,0 +1,23 @@
+package com.sksamuel.avro4s.github
+
+import com.sksamuel.avro4s.{Decoder, TypeGuardedDecoding}
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+import scala.reflect.ClassTag
+import scala.reflect.runtime.universe._
+
+import scala.collection.JavaConverters._
+
+class GithubIssue545 extends AnyWordSpec with Matchers {
+  "TypeGuardedDecoding" should {
+    "create a map decoder instead of an array decoder" in {
+      val mapDecoder = Decoder.mapDecoder[String]
+
+      val typeGuard = TypeGuardedDecoding.guard[Map[String, String]](mapDecoder)
+      val value = Map().asJava
+
+      typeGuard.isDefinedAt(value) shouldBe true
+    }
+  }
+}
