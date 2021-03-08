f2cf305bebb495ad1cf1ca7c9313b4e8db40f4cc
Scala Steward <me@scala-steward.org>
2021-03-08 08:57:54 +0100

    Update scalatest to 3.2.6

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object Build extends AutoPlugin {
     val org = "com.sksamuel.avro4s"
     val AvroVersion = "1.9.2"
     val Log4jVersion = "1.2.17"
-    val ScalatestVersion = "3.2.4"
+    val ScalatestVersion = "3.2.6"
     val Slf4jVersion = "1.7.30"
     val Json4sVersion = "3.6.11"
     val CatsVersion = "2.0.0"
