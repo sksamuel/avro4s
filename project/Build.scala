5636df3d8826b6ef615e2446650ab7150a9157b7
Scala Steward <me@scala-steward.org>
2021-05-05 19:08:13 +0200

    Update shapeless to 2.3.5

--- project/Build.scala
+++ project/Build.scala
@@ -12,7 +12,7 @@ object Build extends AutoPlugin {
     val Slf4jVersion = "1.7.30"
     val Json4sVersion = "3.6.11"
     val CatsVersion = "2.0.0"
-    val ShapelessVersion = "2.3.4"
+    val ShapelessVersion = "2.3.5"
     val RefinedVersion = "0.9.24"
     val MagnoliaVersion = "0.17.0"
     val SbtJmhVersion = "0.3.7"
