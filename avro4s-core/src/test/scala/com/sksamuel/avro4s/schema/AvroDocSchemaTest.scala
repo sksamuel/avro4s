4b9bbd2dfa7062754437fd0c0e26582cafd6381e
sksamuel <sam@sksamuel.com>
2021-06-06 21:25:56 -0500

    Restored some tests

--- avro4s-core/src/test/scala/com/sksamuel/avro4s/schema/AvroDocSchemaTest.scala
+++ avro4s-core/src/test/scala/com/sksamuel/avro4s/schema/AvroDocSchemaTest.scala
@@ -27,19 +27,19 @@ class AvroDocSchemaTest extends AnyWordSpec with Matchers {
       val schema = AvroSchema[Annotated]
       schema.toString(true) shouldBe expected.toString(true)
     }
-    //    "support value type when placed at the class level should annotate the field in the final schema" in {
-    //      val expected = new org.apache.avro.Schema.Parser().parse(getClass.getResourceAsStream("/doc_annotation_value_type.json"))
-    //      val schema = AvroSchema[Annotated123]
-    //      schema.toString(true) shouldBe expected.toString(true)
-    //    }
+    // todo once magnolia has value type support
+    // "support value type when placed at the class level should annotate the field in the final schema" in {
+    //   val expected = new org.apache.avro.Schema.Parser().parse(getClass.getResourceAsStream("/doc_annotation_value_type.json"))
+    //   val schema = AvroSchema[Annotated123]
+    //   schema.toString(true) shouldBe expected.toString(true)
+    // }
 
     // todo once magnolia has value type support
-    //    "produce doc only on the field record when field is not a value class" in {
-    //      val expected = new org.apache.avro.Schema.Parser().parse(getClass.getResourceAsStream("/doc_field_regular_case_class.json"))
-    //      val schema = AvroSchema[Message]
-    //
-    //      schema.toString(true) shouldBe expected.toString(true)
-    //    }
+    // "produce doc only on the field record when field is not a value class" in {
+    //   val expected = new org.apache.avro.Schema.Parser().parse(getClass.getResourceAsStream("/doc_field_regular_case_class.json"))
+    //   val schema = AvroSchema[Message]
+    //   schema.toString(true) shouldBe expected.toString(true)
+    //  }
   }
 }
 
