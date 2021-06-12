c2b4225b5e4c9ef24d770834e68a7f9869322e9c
sksamuel <sam@sksamuel.com>
2021-06-12 16:43:57 -0500

    Updated record encoder to respect schema field order

--- avro4s-core/src/main/scala/com/sksamuel/avro4s/encoders/records.scala
+++ avro4s-core/src/main/scala/com/sksamuel/avro4s/encoders/records.scala
@@ -1,57 +1,58 @@
 package com.sksamuel.avro4s.encoders
 
 import com.sksamuel.avro4s.typeutils.{Annotations, Names}
-import com.sksamuel.avro4s.{Encoder, ImmutableRecord, SchemaFor}
+import com.sksamuel.avro4s.{Avro4sConfigurationException, Encoder, ImmutableRecord, SchemaFor}
 import magnolia.CaseClass
 import org.apache.avro.Schema
 
+import scala.collection.mutable
+import scala.jdk.CollectionConverters._
 import scala.reflect.ClassTag
 
 class RecordEncoder[T](ctx: magnolia.CaseClass[Encoder, T]) extends Encoder[T] {
 
   def encode(schema: Schema): T => Any = {
-    val encoders = ctx.params
-      .map { param => new FieldEncoder(param) }
-      .map { it => it.encode(schema) }
-      .toArray
+    // the order of the encoders comes from the schema
+    val encoders: Array[FieldEncoder[T]] = schema.getFields.asScala.map { field =>
+      val param = findParam(field, ctx)
+      if (param.isEmpty)
+        throw new Avro4sConfigurationException(s"Unable to find case class parameter for field ${field.name()}")
+      new FieldEncoder(param.get, field.schema())
+    }.toArray
     { t => encodeT(schema, encoders, t) }
   }
 
-  private def encodeT(schema:Schema, encoders: Array[T => Any], t: T): ImmutableRecord = {
+  /**
+    * Finds the matching param from the case class for the given avro [[Schema.Field]].
+    */
+  private def findParam(field: Schema.Field, ctx: magnolia.CaseClass[Encoder, T]): Option[CaseClass.Param[Encoder, T]] = {
+    ctx.params.find { param =>
+      val annotations = new Annotations(param.annotations)
+      val paramName = annotations.name.getOrElse(param.label)
+      paramName == field.name()
+    }
+  }
+
+  private def encodeT(schema: Schema, encoders: Array[FieldEncoder[T]], t: T): ImmutableRecord = {
     // hot code path. Sacrificing functional programming to the gods of performance.
     val length = encoders.length
     val values = new Array[Any](length)
     var i = 0
     while (i < length) {
-      values(i) = encoders(i).apply(t)
+      values(i) = encoders(i).encode(t)
       i += 1
     }
-    ImmutableRecord(schema, values) // note: array gets implicitly wrapped in an immutable container.
+    ImmutableRecord(schema, values.toSeq)
   }
-
-  //  private def extractField[Typeclass[_]](param: Param[Typeclass, _], schemaFor: SchemaFor[_]): Field = {
-  //    val annotations = new Annotations(param.annotations)
-  //    val fieldName = annotations.name.getOrElse("") // (schemaFor.fieldMapper.to(param.label))
-  //    val field = schemaFor.schema.getField(fieldName)
-  //    if (field == null) {
-  //      throw new Avro4sConfigurationException(
-  //        s"Unable to find field with name $fieldName for case class parameter ${param.label} in schema ${schemaFor.schema}")
-  //    }
-  //    field
-  //  }
 }
 
-class FieldEncoder[T](param: magnolia.CaseClass.Param[Encoder, T]) extends Encoder[T] with Serializable :
+class FieldEncoder[T](param: magnolia.CaseClass.Param[Encoder, T], schema: Schema) extends Serializable :
 
-  private val name = Annotations(param.annotations).name.getOrElse(param.label)
+  private val encode = param.typeclass.encode(schema)
 
-  override def encode(schema: Schema): T => Any = {
-    val fieldSchema = schema.getField(name).schema()
-    val encoderF = param.typeclass.encode(fieldSchema)
-    { t =>
-      val value = param.deref(t)
-      encoderF.apply(value)
-    }
+  def encode(record: T): Any = {
+    val value = param.deref(record)
+    encode.apply(value)
   }
 
 //  // using inner class here to be able to reference param.PType below, and keep the type relation intact in
@@ -67,25 +68,3 @@ class FieldEncoder[T](param: magnolia.CaseClass.Param[Encoder, T]) extends Encod
 //            record: Schema,
 //            ctx: CaseClass[Encoder, T],
 //            fieldMapper: FieldMapper): (Field, ValueEncoder) = {
-//
-//    val (encoder, field) = update match {
-//      case FullSchemaUpdate(sf) =>
-//        // in a full schema update, the schema is the leading information and we derive encoder modifications from it.
-//        // so we extract the field and create a schema update from its schema and apply it to the encoder
-//        // via resolveEncoder.
-//        val field = extractField(param, sf)
-//
-//        val fieldUpdate = FullSchemaUpdate(SchemaFor(field.schema(), sf.fieldMapper))
-//        val encoder = param.typeclass.resolveEncoder(env, fieldUpdate)
-//        (encoder, field)
-//
-//      case _ =>
-//        // Otherwise, we look for annotations on the field (such as AvroFixed or AvroNamespace) and use those to
-//        // compute modifications to apply to the encoder.
-//        // The field schema is then derived from the encoder schema.
-//        val encoder = param.typeclass.resolveEncoder(env, fieldUpdate(param, record, fieldMapper))
-//        (encoder, buildField(param, record, ctx, encoder.schema, fieldMapper))
-//    }
-//
-//    field -> new ValueEncoder(encoder, field.name)
-//  }
