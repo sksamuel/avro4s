4d32f663ef371994b187aba21f92285cb273f15b
sksamuel <sam@sksamuel.com>
2021-06-12 16:29:31 -0500

    Added test for deeply nested encoder

--- avro4s-core/src/test/scala/com/sksamuel/avro4s/record/encoder/NestedStructEncoderTest.scala
+++ avro4s-core/src/test/scala/com/sksamuel/avro4s/record/encoder/NestedStructEncoderTest.scala
@@ -1,30 +1,30 @@
-//package com.sksamuel.avro4s.record.encoder
-//
-//import com.sksamuel.avro4s.{AvroSchema, Encoder, ImmutableRecord}
-//import org.apache.avro.util.Utf8
-//import org.scalatest.funsuite.AnyFunSuite
-//import org.scalatest.matchers.should.Matchers
-//
-//class NestedStructEncoderTest extends AnyFunSuite with Matchers {
-//
-//  test("encode nested structs") {
-//
-//    case class Foo(s: String)
-//    case class Fooo(foo: Foo)
-//    case class Foooo(fooo: Fooo)
-//
-//    Encoder[Foooo].encode(Foooo(Fooo(Foo("a")))) shouldBe
-//      ImmutableRecord(
-//        AvroSchema[Foooo],
-//        Vector(
-//          ImmutableRecord(
-//            AvroSchema[Fooo],
-//            Vector(
-//              ImmutableRecord(
-//                AvroSchema[Foo],
-//                Vector(new Utf8("a"))))
-//          )
-//        )
-//      )
-//  }
-//}
+package com.sksamuel.avro4s.record.encoder
+
+import com.sksamuel.avro4s.{AvroSchema, Encoder, ImmutableRecord}
+import org.apache.avro.util.Utf8
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class NestedStructEncoderTest extends AnyFunSuite with Matchers {
+
+  test("encode nested structs") {
+
+    case class Foo(s: String)
+    case class Fooo(foo: Foo)
+    case class Foooo(fooo: Fooo)
+
+    Encoder[Foooo].encode(AvroSchema[Foooo]).apply(Foooo(Fooo(Foo("a")))) shouldBe
+      ImmutableRecord(
+        AvroSchema[Foooo],
+        Vector(
+          ImmutableRecord(
+            AvroSchema[Fooo],
+            Vector(
+              ImmutableRecord(
+                AvroSchema[Foo],
+                Vector(new Utf8("a"))))
+          )
+        )
+      )
+  }
+}
