b9f4db6ae728984a79a2e363c80531f722c0f9f1
sksamuel <sam@sksamuel.com>
2021-06-12 21:06:36 -0500

    Restored some stream tests

--- avro4s-core/src/test/scala/com/sksamuel/avro4s/streams/input/BasicInputStreamTest.scala
+++ avro4s-core/src/test/scala/com/sksamuel/avro4s/streams/input/BasicInputStreamTest.scala
@@ -1,45 +1,45 @@
-//package com.sksamuel.avro4s.streams.input
-//
-//import com.sksamuel.avro4s._
-//
-//import scala.util.Failure
-//
-//class BasicInputStreamTest extends InputStreamTest {
-//
-//  case class BooleanTest(z: Boolean)
-//  case class StringTest(z: String)
-//  case class FloatTest(z: Float)
-//  case class DoubleTest(z: Double)
-//  case class IntTest(z: Int)
-//  case class LongTest(z: Long)
-//
-//  test("read write out booleans") {
-//    writeRead(BooleanTest(true))
-//  }
-//
-//  test("read and skip over corrupted data") {
-//    val items = tryReadData[StringTest](writeData(FloatTest(3.4F)).toByteArray).toSeq
-//    items.size shouldBe 1
-//    items.head shouldBe a[Failure[_]]
-//  }
-//
-//  test("read write out strings") {
-//    writeRead(StringTest("Hello world"))
-//  }
-//
-//  test("read write out longs") {
-//    writeRead(LongTest(65653L))
-//  }
-//
-//  test("read write out ints") {
-//    writeRead(IntTest(44))
-//  }
-//
-//  test("read write out doubles") {
-//    writeRead(DoubleTest(3.235))
-//  }
-//
-//  test("read write out floats") {
-//    writeRead(FloatTest(3.4F))
-//  }
-//}
+package com.sksamuel.avro4s.streams.input
+
+import com.sksamuel.avro4s._
+
+import scala.util.Failure
+
+class BasicInputStreamTest extends InputStreamTest {
+
+  case class BooleanTest(z: Boolean)
+  case class StringTest(z: String)
+  case class FloatTest(z: Float)
+  case class DoubleTest(z: Double)
+  case class IntTest(z: Int)
+  case class LongTest(z: Long)
+
+  test("read write out booleans") {
+    writeRead(BooleanTest(true))
+  }
+
+  test("read and skip over corrupted data") {
+    val items = tryReadData[StringTest](writeData(FloatTest(3.4F)).toByteArray).toSeq
+    items.size shouldBe 1
+    items.head shouldBe a[Failure[_]]
+  }
+
+  test("read write out strings") {
+    writeRead(StringTest("Hello world"))
+  }
+
+  test("read write out longs") {
+    writeRead(LongTest(65653L))
+  }
+
+  test("read write out ints") {
+    writeRead(IntTest(44))
+  }
+
+  test("read write out doubles") {
+    writeRead(DoubleTest(3.235))
+  }
+
+  test("read write out floats") {
+    writeRead(FloatTest(3.4F))
+  }
+}
--- avro4s-core/src/test/scala/com/sksamuel/avro4s/streams/input/CollectionsInputStreamTest.scala
+++ avro4s-core/src/test/scala/com/sksamuel/avro4s/streams/input/CollectionsInputStreamTest.scala
@@ -51,21 +51,21 @@ class CollectionsInputStreamTest extends InputStreamTest {
     writeRead(ListBooleans(List(true, false, true)))
   }
 
-//  test("read write map of strings") {
-//    writeRead(MapStrings(Map("a" -> "z", "c" -> "d")))
-//  }
-//
-//  test("read write map of doubles") {
-//    writeRead(MapDoubles(Map("a" -> 2914.4, "b" -> 92374.34)))
-//  }
-//
-//  test("read write map of booleans") {
-//    writeRead(MapBooleans(Map("a" -> true, "b" -> false)))
-//  }
-//
-//  test("read write map of nested classes") {
-//    writeRead(MapWithNestedClasses(Map("foo" -> FooString("sam"))))
-//  }
+  test("read write map of strings") {
+    writeRead(MapStrings(Map("a" -> "z", "c" -> "d")))
+  }
+
+  test("read write map of doubles") {
+    writeRead(MapDoubles(Map("a" -> 2914.4, "b" -> 92374.34)))
+  }
+
+  test("read write map of booleans") {
+    writeRead(MapBooleans(Map("a" -> true, "b" -> false)))
+  }
+
+  test("read write map of nested classes") {
+    writeRead(MapWithNestedClasses(Map("foo" -> FooString("sam"))))
+  }
 
   test("read write Set of nested classes") {
     writeRead(SetFoo(Set(FooString("a"), FooString("b"))))
@@ -88,30 +88,30 @@ class CollectionsInputStreamTest extends InputStreamTest {
   }
 
   test("read write top level Vector[Double]") {
-    writeRead(Vector(5.55,2.43,9.9999999))
+    writeRead(Vector(5.55, 2.43, 9.9999999))
   }
 
-//  test("read write top level Map[String, Int]") {
-//    val data = Map("a" -> 111, "ç" -> 222, "阿夫罗" -> 333)
-//    writeRead(data)
-//  }
-//
-//  test("read write top level Map[String, Double]") {
-//    val data = Map("a" -> 1.2, "ç" -> 34.5, "阿夫罗" -> 54.3)
-//    writeRead(data)
-//  }
-//
-//  test("read write top level Map[String, String]") {
-//    val data = Map("a" -> "b", "ç" -> "đ", "阿夫罗" -> "아브로")
-//    writeRead(data)
-//  }
-//
-//  test("read write top level Map[String, MapWithNestedClasses]") {
-//    val data = Map(
-//      "a" -> MapWithNestedClasses(Map("1" -> FooString("b"), "1a" -> FooString("Yolo"))),
-//      "ç" -> MapWithNestedClasses(Map("2" -> FooString("đ"))),
-//      "阿夫罗" -> MapWithNestedClasses(Map("3" -> FooString("아브로")))
-//    )
-//    writeRead(data)
-//  }
+  test("read write top level Map[String, Int]") {
+    val data = Map("a" -> 111, "ç" -> 222, "阿夫罗" -> 333)
+    writeRead(data)
+  }
+
+  test("read write top level Map[String, Double]") {
+    val data = Map("a" -> 1.2, "ç" -> 34.5, "阿夫罗" -> 54.3)
+    writeRead(data)
+  }
+
+  test("read write top level Map[String, String]") {
+    val data = Map("a" -> "b", "ç" -> "đ", "阿夫罗" -> "아브로")
+    writeRead(data)
+  }
+
+  test("read write top level Map[String, MapWithNestedClasses]") {
+    val data = Map(
+      "a" -> MapWithNestedClasses(Map("1" -> FooString("b"), "1a" -> FooString("Yolo"))),
+      "ç" -> MapWithNestedClasses(Map("2" -> FooString("đ"))),
+      "阿夫罗" -> MapWithNestedClasses(Map("3" -> FooString("아브로")))
+    )
+    writeRead(data)
+  }
 }
