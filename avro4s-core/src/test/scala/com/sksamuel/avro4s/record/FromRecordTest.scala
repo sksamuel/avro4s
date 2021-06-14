47e31b7d7662fde68c699f5bb42e130f158d54d9
sksamuel <sam@sksamuel.com>
2021-06-13 21:18:43 -0500

    Added to record tests

---
+++ avro4s-core/src/test/scala/com/sksamuel/avro4s/record/FromRecordTest.scala
@@ -0,0 +1,32 @@
+package com.sksamuel.avro4s.record
+
+import com.sksamuel.avro4s.{AvroSchema, FromRecord}
+import org.apache.avro.generic.GenericData
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+case class HasSomeFields(str: String, int: Int, boolean: Boolean, nested: Nested)
+case class Nested(foo: String)
+case class HasLessFields(str: String, boolean: Boolean, nested: Nested)
+
+class FromRecordTest extends AnyWordSpec with Matchers {
+
+  "FromRecord" should {
+    "decode to class with a subset of fields used to encode" in {
+      val schema = AvroSchema[HasSomeFields]
+      val nestedSchema = AvroSchema[Nested]
+
+      val record = new GenericData.Record(schema)
+      val nestedRecord = new GenericData.Record(nestedSchema)
+      record.put("str", "hello")
+      record.put("int", 42)
+      record.put("boolean", false)
+      nestedRecord.put("foo", "there")
+      record.put("nested", nestedRecord)
+
+      FromRecord[HasLessFields](schema).from(record) shouldBe HasLessFields("hello", false, Nested("there"))
+    }
+  }
+}
+
+
--- avro4s-core/src/test/scala/com/sksamuel/avro4s/record/ToRecordTest.scala
+++ avro4s-core/src/test/scala/com/sksamuel/avro4s/record/ToRecordTest.scala
@@ -1,19 +1,31 @@
-//package com.sksamuel.avro4s.record
-//
-//import com.sksamuel.avro4s.{AvroNamespace, ToRecord}
-//import org.scalatest.funsuite.AnyFunSuite
-//import org.scalatest.matchers.should.Matchers
-//
-//sealed trait Foo
-//case class Bar(i: Int) extends Foo
-//case class Baz(s: String) extends Foo
-//
-//case class MySchema(@AvroNamespace("broken") foo: Foo, id: String, x: Int)
-//
-//class ToRecordTest extends AnyFunSuite with Matchers {
-//
-//  test("ToRecord should work with a namespace annotation on an ADT") {
-//    val ms = MySchema(Bar(1), "", 0)
-//    ToRecord[MySchema].to(ms) //throws
-//  }
-//}
+package com.sksamuel.avro4s.record
+
+import com.sksamuel.avro4s.{AvroNamespace, AvroSchema, FromRecord, ToRecord}
+import org.apache.avro.generic.{GenericData, GenericRecord}
+import org.apache.avro.util.Utf8
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+sealed trait Foo
+case class Bar(i: Int) extends Foo
+case class Baz(s: String) extends Foo
+
+case class MySchema(@AvroNamespace("broken") foo: Foo, id: String, x: Int)
+
+class ToRecordTest extends AnyFunSuite with Matchers {
+
+  test("encode to record") {
+    val schema = AvroSchema[HasSomeFields]
+    val record = ToRecord[HasSomeFields](schema).to(HasSomeFields("hello", 42, false, Nested("there")))
+    record.get("str") shouldBe new Utf8("hello")
+    record.get("int").asInstanceOf[Int] shouldBe 42
+    record.get("boolean").asInstanceOf[Boolean] shouldBe false
+    record.get("nested").asInstanceOf[GenericRecord].get("foo") shouldBe new Utf8("there")
+  }
+
+  //  test("ToRecord should work with a namespace annotation on an ADT") {
+  //    val schema = AvroSchema[MySchema]
+  //    val ms = MySchema(Bar(1), "", 0)
+  //    ToRecord[MySchema](schema).to(ms)
+  //  }
+}
