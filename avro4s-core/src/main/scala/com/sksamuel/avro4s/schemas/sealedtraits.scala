9678d2c4d56e93d1c5bf0fbe02eb004a5ae16546
Alexander Lomov <alexander.lomov.osv@fedex.com>
2021-12-31 12:05:06 +0100

    Work around a problem that annotations of the enum declaration get attached to the enum elements, causing duplicate enum items in the schema.

--- avro4s-core/src/main/scala/com/sksamuel/avro4s/schemas/sealedtraits.scala
+++ avro4s-core/src/main/scala/com/sksamuel/avro4s/schemas/sealedtraits.scala
@@ -1,6 +1,6 @@
 package com.sksamuel.avro4s.schemas
 
-import com.sksamuel.avro4s.SchemaFor
+import com.sksamuel.avro4s.{AvroName, SchemaFor}
 import com.sksamuel.avro4s.typeutils.{Annotations, Names, SubtypeOrdering}
 import magnolia1.SealedTrait
 import org.apache.avro.{Schema, SchemaBuilder}
@@ -8,19 +8,25 @@ import org.apache.avro.{Schema, SchemaBuilder}
 object SealedTraits {
   def schema[T](ctx: SealedTrait[SchemaFor, T]): Schema = {
 
-    ctx.subtypes.sorted(SubtypeOrdering).foreach { st =>
-      Console.err.println(st.annotations.collectFirst( {
-        case a: com.sksamuel.avro4s.AvroNamespace => a
-        case a: com.sksamuel.avro4s.AvroName => a
-      }
-    ))
-    }
+    val names = Names(ctx.typeInfo, Annotations(ctx.annotations))
 
+    // It currently appears (magnolia1, v 1) that all the enum elements carry the same annotation set
+    // as the enumeration symbol itself, which leads to name clash in the generated schema. 
+    // For now I'm disabling AvroName annotation interpretation of the symbols
+    // if its name equals to the whole enumeration name.
+    // Annotaions that are attached to the enum elements are not visible here.
+    // Looks lilke we ned to have a look into either Magnolia or Scala 3.
     val symbols = ctx.subtypes.sorted(SubtypeOrdering).map { st =>
-      Names(st.typeInfo, Annotations(st.annotations)).name
+      Names(
+        st.typeInfo,
+        Annotations(
+          st.annotations.filter {
+            case an: AvroName => an.name != names.name
+            case _ => true
+          }
+        )
+      ).name
     }
-
-    val names = Names(ctx.typeInfo, Annotations(ctx.annotations))   
 
     SchemaBuilder.enumeration(names.name).namespace(names.namespace).symbols(symbols*)
 
--- avro4s-core/src/test/scala/com/sksamuel/avro4s/record/encoder/Scala3EnumEncoderTest.scala
+++ avro4s-core/src/test/scala/com/sksamuel/avro4s/record/encoder/Scala3EnumEncoderTest.scala
@@ -1,6 +1,6 @@
 package com.sksamuel.avro4s.record.encoder
 
-import com.sksamuel.avro4s.{Avro4sException, AvroSchema, Decoder, Encoder}
+import com.sksamuel.avro4s.{Avro4sException, AvroName, AvroNamespace, AvroSchema, Decoder, Encoder}
 import org.apache.avro.generic.{GenericData, GenericRecord}
 import org.apache.avro.util.Utf8
 import org.scalatest.funsuite.AnyFunSuite
@@ -17,14 +17,14 @@ class Scala3EnumEncoderTest extends AnyFunSuite with Matchers {
     record.toString shouldBe "ONE"
   }
 
-  test("supoort non-parametrized, extending Scala 3 enums") {
+  test("support non-parametrized, extending Scala 3 enums") {
     val schema = AvroSchema[Extending]
     val record = Encoder[Extending].encode(schema).apply(Extending.TWO).asInstanceOf[GenericData.EnumSymbol]
     schema.getType shouldBe org.apache.avro.Schema.Type.ENUM
     record.toString shouldBe "TWO"
   }
 
-  test("supoort parametrized Scala 3 enums") {
+  test("support parametrized Scala 3 enums") {
     val schema = AvroSchema[ParametrizedWithInt]
     val record = Encoder[ParametrizedWithInt].encode(schema).apply(ParametrizedWithInt.THREE).asInstanceOf[GenericData.EnumSymbol]
     schema.getType shouldBe org.apache.avro.Schema.Type.ENUM
@@ -37,7 +37,32 @@ class Scala3EnumEncoderTest extends AnyFunSuite with Matchers {
     schema.getType shouldBe org.apache.avro.Schema.Type.ENUM
     schema.getNamespace shouldBe "com.test.namespace"
     schema.getName shouldBe "CustomSimple"
-    record.toString shouldBe "ONE"
+    schema.getEnumSymbols should contain("ONE")
+  }
+
+  test("support non-parametrized, extending Scala 3 annotated enums") {
+    val schema = AvroSchema[ExtendingAnnotated]
+    val record = Encoder[ExtendingAnnotated].encode(schema).apply(ExtendingAnnotated.TWO).asInstanceOf[GenericData.EnumSymbol]
+    schema.getType shouldBe org.apache.avro.Schema.Type.ENUM
+    schema.getNamespace shouldBe "com.test.namespace"
+    schema.getName shouldBe "CustomExtended"
+    schema.getEnumSymbols should contain("TWO")
+  }
+
+  test("support parametrized Scala 3 annotated enums") {
+    val schema = AvroSchema[ParametrizedWithIntAnnotated]
+    val record = Encoder[ParametrizedWithIntAnnotated].encode(schema).apply(ParametrizedWithIntAnnotated.THREE).asInstanceOf[GenericData.EnumSymbol]
+    schema.getType shouldBe org.apache.avro.Schema.Type.ENUM
+    schema.getNamespace shouldBe "com.test.namespace"
+    schema.getName shouldBe "CustomParametrizedWithInt"
+    record.toString shouldBe "THREE"
+  }
+
+  test("support Scala 3 enums that are parametrized with a product") {
+    val schema = AvroSchema[WithProduct]
+    val record = Encoder[WithProduct].encode(schema).apply(WithProduct.One).asInstanceOf[GenericData.EnumSymbol]
+    schema.getType shouldBe org.apache.avro.Schema.Type.ENUM
+    record.toString shouldBe "One"
   }
   
 }
@@ -58,24 +83,32 @@ enum ParametrizedWithInt(val param: Int) {
   case THREE extends ParametrizedWithInt(3)
 }
 
-@com.sksamuel.avro4s.AvroNamespace("com.test.namespace")
-@com.sksamuel.avro4s.AvroName("CustomSimple")
+@AvroNamespace("com.test.namespace")
+@AvroName("CustomSimple")
 enum SimpleAnnotated {
   case ONE, TWO, THREE
 }
 
-@com.sksamuel.avro4s.AvroNamespace("com.test.namespace")
-@com.sksamuel.avro4s.AvroName("CustomExtended")
+@AvroNamespace("com.test.namespace")
+@AvroName("CustomExtended")
 enum ExtendingAnnotated {
   case ONE extends ExtendingAnnotated
   case TWO extends ExtendingAnnotated
   case THREE extends ExtendingAnnotated
 }
 
-@com.sksamuel.avro4s.AvroNamespace("com.test.namespace")
-@com.sksamuel.avro4s.AvroName("CustomParametrizedWithInt")
+@AvroNamespace("com.test.namespace")
+@AvroName("CustomParametrizedWithInt")
 enum ParametrizedWithIntAnnotated(val param: Int) {
   case ONE extends ParametrizedWithIntAnnotated(1)
   case TWO extends ParametrizedWithIntAnnotated(2)
   case THREE extends ParametrizedWithIntAnnotated(3)
+}
+
+
+final case class TestProduct(a: String, b: Int)
+
+enum WithProduct(product: TestProduct) {
+  case One extends WithProduct(TestProduct("a", 1))
+  case Two extends WithProduct(TestProduct("b", 2))
 }
