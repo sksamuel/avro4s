8d214475c51b2fba2e74503b73270e18e9e22a2f
Scala Steward <scala_steward@virtuslab.com>
2022-08-22 16:23:48 +0000

    Update log4j-over-slf4j, slf4j-api to 2.0.0

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ object Build extends AutoPlugin {
     val AvroVersion = "1.11.0"
     val Log4jVersion = "1.2.17"
     val ScalatestVersion = "3.2.12"
-    val Slf4jVersion = "1.7.36"
+    val Slf4jVersion = "2.0.0"
     val Json4sVersion = "3.6.11"
     val CatsVersion = "2.7.0"
     val RefinedVersion = "0.9.26"
