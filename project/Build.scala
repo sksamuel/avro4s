91a1895262e9e27c1acf6fa14a4b1372e81ac7aa
aarushiavlino <110448379+aarushiavlino@users.noreply.github.com>
2022-12-22 13:21:40 +0530

    As log4j version 1.2.17 have vulnerability (in test dependencies) updated version to 2.17.1 which does not have vulnerability

--- project/Build.scala
+++ project/Build.scala
@@ -6,63 +6,50 @@ object Build extends AutoPlugin {
 
   object autoImport {
     val org = "com.sksamuel.avro4s"
-    val AvroVersion = "1.9.2"
-    val Log4jVersion = "1.2.17"
-    val ScalatestVersion = "3.2.9"
-    val Slf4jVersion = "1.7.30"
+    val AvroVersion = "1.11.0"
+    val Log4jVersion = "2.17.1"
+    val ScalatestVersion = "3.2.14"
+    val Slf4jVersion = "2.0.6"
     val Json4sVersion = "3.6.11"
-    val CatsVersion = "2.0.0"
+    val CatsVersion = "2.7.0"
     val RefinedVersion = "0.9.26"
     val ShapelessVersion = "2.3.7"
-    val MagnoliaVersion = "0.17.0"
+    val MagnoliaVersion = "1.1.4"
     val SbtJmhVersion = "0.3.7"
-    val JmhVersion = "1.23"
+    val JmhVersion = "1.32"
   }
 
   import autoImport._
 
-  def isGithubActions = sys.env.getOrElse("CI", "false") == "true"
-  def releaseVersion = sys.env.getOrElse("RELEASE_VERSION", "")
-  def isRelease = releaseVersion != ""
-  def githubRunNumber = sys.env.getOrElse("GITHUB_RUN_NUMBER", "local")
-  def ossrhUsername = sys.env.getOrElse("OSSRH_USERNAME", "")
-  def ossrhPassword = sys.env.getOrElse("OSSRH_PASSWORD", "")
-  def publishVersion = if (isRelease) releaseVersion else "4.1.0." + githubRunNumber + "-SNAPSHOT"
+  def isGithubActions: Boolean = sys.env.getOrElse("CI", "false") == "true"
+  def releaseVersion: String = sys.env.getOrElse("RELEASE_VERSION", "")
+  def isRelease: Boolean = releaseVersion != ""
+  def githubRunNumber: String = sys.env.getOrElse("GITHUB_RUN_NUMBER", "local")
+  def ossrhUsername: String = sys.env.getOrElse("OSSRH_USERNAME", "")
+  def ossrhPassword: String = sys.env.getOrElse("OSSRH_PASSWORD", "")
+  def publishVersion: String = if (isRelease) releaseVersion else "5.0.0." + githubRunNumber + "-SNAPSHOT"
 
   override def trigger = allRequirements
   override def projectSettings = publishingSettings ++ Seq(
     organization := org,
-    scalaVersion := "2.13.5",
-    crossScalaVersions := Seq("2.12.10", "2.13.5"),
+    scalaVersion := "3.2.1",
     resolvers += Resolver.mavenLocal,
-    parallelExecution in Test := false,
-    scalacOptions := Seq(
-      "-unchecked", "-deprecation",
-      "-encoding",
-      "utf8",
-   //   "-Xfatal-warnings",
-      "-feature",
-      "-language:higherKinds",
-   //   "-Xlog-implicits",
-      "-language:existentials",
-      "-Ybackend-parallelism",
-      "8"
-    ),
+    Test / parallelExecution := false,
+    Test / scalacOptions ++= Seq("-Xmax-inlines:64"),
     javacOptions := Seq("-source", "1.8", "-target", "1.8"),
     libraryDependencies ++= Seq(
-      "org.scala-lang"    % "scala-reflect"     % scalaVersion.value,
-      "org.scala-lang"    % "scala-compiler"    % scalaVersion.value,
-      "org.apache.avro"   % "avro"              % AvroVersion,
-      "org.slf4j"         % "slf4j-api"         % Slf4jVersion          % "test",
-      "log4j"             % "log4j"             % Log4jVersion          % "test",
-      "org.slf4j"         % "log4j-over-slf4j"  % Slf4jVersion          % "test",
-      "org.scalatest"     %% "scalatest"        % ScalatestVersion      % "test"
+      "org.scala-lang"           % "scala3-compiler_3" % scalaVersion.value,
+      "org.apache.avro"          % "avro"              % AvroVersion,
+      "org.slf4j"                % "slf4j-api"         % Slf4jVersion          % "test",
+      "org.apache.logging.log4j" % "log4j-core"        % Log4jVersion          % "test",
+      "org.slf4j"                % "log4j-over-slf4j"  % Slf4jVersion          % "test",
+      "org.scalatest"            % "scalatest_3"       % ScalatestVersion      % "test"
     )
   )
 
   val publishingSettings = Seq(
     publishMavenStyle := true,
-    publishArtifact in Test := false,
+    Test / publishArtifact := false,
     credentials += Credentials(
       "Sonatype Nexus Repository Manager",
       "oss.sonatype.org",
@@ -82,8 +69,8 @@ object Build extends AutoPlugin {
       <url>https://github.com/sksamuel/avro4s</url>
         <licenses>
           <license>
-            <name>MIT</name>
-            <url>https://opensource.org/licenses/MIT</url>
+            <name>The Apache 2.0 License</name>
+            <url>https://opensource.org/licenses/Apache-2.0</url>
             <distribution>repo</distribution>
           </license>
         </licenses>
