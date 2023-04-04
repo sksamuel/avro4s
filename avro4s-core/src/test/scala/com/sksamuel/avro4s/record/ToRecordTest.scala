b5c7dcdcb1b4460dea697bcc3561c73c20b4b9a1
Mariusz Ołownia <mariusz.olownia@scalac.io>
2023-04-04 15:03:29 +0200

    New test in ToRecordTest

--- avro4s-core/src/test/scala/com/sksamuel/avro4s/record/ToRecordTest.scala
+++ avro4s-core/src/test/scala/com/sksamuel/avro4s/record/ToRecordTest.scala
@@ -1,6 +1,6 @@
 package com.sksamuel.avro4s.record
 
-import com.sksamuel.avro4s.{AvroNamespace, AvroSchema, FromRecord, ToRecord}
+import com.sksamuel.avro4s.{AvroNamespace, AvroSchema, ImmutableRecord, Record, FromRecord, ToRecord}
 import org.apache.avro.generic.{GenericData, GenericRecord}
 import org.apache.avro.util.Utf8
 import org.scalatest.funsuite.AnyFunSuite
@@ -21,6 +21,17 @@ class ToRecordTest extends AnyFunSuite with Matchers {
     record.get("int").asInstanceOf[Int] shouldBe 42
     record.get("boolean").asInstanceOf[Boolean] shouldBe false
     record.get("nested").asInstanceOf[GenericRecord].get("foo") shouldBe new Utf8("there")
+  }
+
+  test("encode to record with sealed trait") {
+    val schema = AvroSchema[Foo]
+    val record = ToRecord[Foo](schema).to(Bar(1))
+
+    record.get("i").asInstanceOf[Int] shouldBe 1
+
+    record match
+      case r: ImmutableRecord => r.schema shouldBe schema
+      case _: Record => throw new Exception
   }
 
   //  test("ToRecord should work with a namespace annotation on an ADT") {
