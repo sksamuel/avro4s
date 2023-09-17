7c11dbbf430e1a557986cc906a63123d04fdcf7a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-09-17 22:21:51 +0200

    Update log4j-over-slf4j, slf4j-api to 2.0.9 (#795)

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ object Build extends AutoPlugin {
     val AvroVersion = "1.11.0"
     val Log4jVersion = "1.2.17"
     val ScalatestVersion = "3.2.16"
-    val Slf4jVersion = "2.0.7"
+    val Slf4jVersion = "2.0.9"
     val Json4sVersion = "3.6.11"
     val CatsVersion = "2.7.0"
     val RefinedVersion = "0.9.26"
