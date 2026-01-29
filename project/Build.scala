4869c5c067ee9372dd6f10e9d4d96d781d626bf0
Scala Steward <scala_steward@virtuslab.com>
2026-01-29 12:23:59 +0000

    Update avro to 1.12.1

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import sbt.*
 object Build extends AutoPlugin {
 
   object autoImport {
-    val AvroVersion = "1.11.5"
+    val AvroVersion = "1.12.1"
     val Log4jVersion = "1.2.17"
     val ScalatestVersion = "3.2.17"
     val Slf4jVersion = "2.0.17"
